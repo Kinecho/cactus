583da3c7a1787931c3a2e13c34b2d02e12a7cb7a
Katie Blackman <katie@kinecho.com>
2019-06-10 10:33:47 -0600

    what activity gives you the most physical energy

--- web/pages.js
+++ web/pages.js
@@ -73,5 +73,9 @@ module.exports = {
     "what_is_your_most_cherished_memory_you_have_with_another_person": {
         "title": "What is your most cherished memory you have with another person?",
         "path": "/what-is-your-most-cherished-memory-you-have-with-another-person"
+    },
+    "what_activity_gives_you_the_most_physical_energy": {
+        "title": "What activity gives you the most physical energy?",
+        "path": "/what-activity-gives-you-the-most-physical-energy"
     }
 }
---
+++ web/src/scripts/pages/what_activity_gives_you_the_most_physical_energy.ts
@@ -0,0 +1,9 @@
+import "styles/pages/what_activity_gives_you_the_most_physical_energy.scss"
+import {configureSignupForm, setupJumpToForm} from '@web/mailchimp'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("what_activity_gives_you_the_most_physical_energy loaded");
+    setupJumpToForm();
+    configureSignupForm("email-form-bottom");
+});
