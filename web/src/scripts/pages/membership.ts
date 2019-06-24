ff77f6d8c02d93fc4a63f0df6d8c2c6ee0658aa4
Katie Blackman <katie@kinecho.com>
2019-06-24 12:50:47 -0600

    more responsive

--- web/src/scripts/pages/membership.ts
+++ web/src/scripts/pages/membership.ts
@@ -11,17 +11,43 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function configureAnimations() {
+
+    var width = window.innerWidth,
+    height = window.innerHeight;
+
+    if (width >= 1140) {
+      new ScrollMagic.Scene({
+          offset: 1,
+          duration: '100%',
+      }).setTween(".reflection", 1, {transform: 'translate(0,0)'})
+          .addTo(controller);
+    } else if (width < 1140) {
+      new ScrollMagic.Scene({
+          offset: 1,
+          duration: '30%',
+      }).setTween(".hero .reflection", 1, {transform: 'translate(-30vw,0) scale(0)', opacity: 0})
+          .addTo(controller);
+    }
+
     new ScrollMagic.Scene({
         offset: 1,
         duration: "100%",
     }).setTween(".hero", 1, {backgroundColor: 'white'})
         .addTo(controller);
 
-    new ScrollMagic.Scene({
-        offset: 1,
-        duration: '100%',
-    }).setTween("#pinkBlob", 1, {transform: 'translate(81vw, 140vh)'})
-        .addTo(controller);
+    if (width >= 1140) {
+      new ScrollMagic.Scene({
+          offset: 1,
+          duration: '100%',
+      }).setTween("#pinkBlob", 1, {transform: 'translate(81vw, 140vh)'})
+          .addTo(controller);
+    } else if (width < 1140) {
+      new ScrollMagic.Scene({
+          offset: 1,
+          duration: '100%',
+      }).setTween("#pinkBlob", 1, {transform: 'translate(81vw, 60vh)'})
+          .addTo(controller);
+    }
 
     new ScrollMagic.Scene({
         offset: 1,
@@ -29,33 +55,17 @@ function configureAnimations() {
     }).setTween("#yellowBlob2", 1, {transform: 'translate(-7vw, 99vh) rotate(57deg)'})
         .addTo(controller);
 
-    new ScrollMagic.Scene({
-        offset: 1,
-        duration: '100%',
-    }).setTween("#greenBlob", 1, {transform: 'translate(-12vw, 110vh)'})
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        offset: 1,
-        duration: '100%',
-    }).setTween("#reflection1", 1, {transform: 'translate(0,0)'})
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        offset: 1,
-        duration: '100%',
-    }).setTween("#reflection2", 1, {transform: 'translate(0,0)'})
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        offset: 1,
-        duration: '100%',
-    }).setTween("#reflection3", 1, {transform: 'translate(0,0)'})
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        offset: 1,
-        duration: '100%',
-    }).setTween("#reflection4", 1, {transform: 'translate(0,0)'})
-        .addTo(controller);
+    if (width >= 1140) {
+      new ScrollMagic.Scene({
+          offset: 1,
+          duration: '100%',
+      }).setTween("#greenBlob", 1, {transform: 'translate(-12vw, 110vh)'})
+          .addTo(controller);
+    } else if (width < 1140) {
+      new ScrollMagic.Scene({
+          offset: 1,
+          duration: '100%',
+      }).setTween("#greenBlob", 1, {transform: 'translate(-24vw, 65vh)'})
+          .addTo(controller);
+    }
 }
