1f5e017fe57b914f9871874fb711b5a58bad1399
Katie Blackman <katie@kinecho.com>
2019-06-24 11:41:50 -0600

    more scrollmagic

--- web/src/scripts/pages/membership.ts
+++ web/src/scripts/pages/membership.ts
@@ -8,59 +8,54 @@ const controller = new ScrollMagic.Controller();
 document.addEventListener('DOMContentLoaded', () => {
     console.log("membership loaded");
     configureAnimations();
-
 });
 
 function configureAnimations() {
     new ScrollMagic.Scene({
-        // triggerElement: "body"
+        offset: 1,
+        duration: "100%",
+    }).setTween(".hero", 1, {backgroundColor: 'white'})
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
         offset: 1,
         duration: '100%',
-    }).setClassToggle("#reflection1", "animate")
+    }).setTween("#pinkBlob", 1, {transform: 'translate(81vw, 140vh)'})
         .addTo(controller);
 
     new ScrollMagic.Scene({
-        // offset: 1,
-        triggerElement: "section.truck",
-        duration: "100%",
-    }).setTween(".foodtruck", 0.5, {scale: 2})
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        // triggerElement: "body"
-        offset: 66,
+        offset: 1,
         duration: '100%',
-    }).setTween("#reflection2", 0.5, {scale: 2.0})
+    }).setTween("#yellowBlob2", 1, {transform: 'translate(-7vw, 99vh) rotate(57deg)'})
         .addTo(controller);
 
     new ScrollMagic.Scene({
-        // triggerElement: "body"
-        offset: 132,
+        offset: 1,
         duration: '100%',
-    }).setClassToggle("#reflection3", "animate")
+    }).setTween("#greenBlob", 1, {transform: 'translate(-12vw, 110vh)'})
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        offset: 1,
+        duration: '100%',
+    }).setTween("#reflection1", 1, {transform: 'translate(0,0)'})
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        offset: 1,
+        duration: '100%',
+    }).setTween("#reflection2", 1, {transform: 'translate(0,0)'})
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        offset: 1,
+        duration: '100%',
+    }).setTween("#reflection3", 1, {transform: 'translate(0,0)'})
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        offset: 1,
+        duration: '100%',
+    }).setTween("#reflection4", 1, {transform: 'translate(0,0)'})
         .addTo(controller);
 }
-
-// window.onscroll = function() {
-//   fixedHero();
-// };
-
-function fixedHero() {
-    if (document.body.scrollTop >= 0 || document.documentElement.scrollTop >= 0) {
-        document.getElementById("reflection1").classList.add("animate");
-    }
-    ;
-    if (document.body.scrollTop >= 66 || document.documentElement.scrollTop >= 66) {
-        document.getElementById("reflection2").classList.add("animate");
-    }
-    ;
-    if (document.body.scrollTop >= 132 || document.documentElement.scrollTop >= 132) {
-        document.getElementById("reflection3").classList.add("animate");
-        document.getElementById("stem2").classList.add("fadeOut");
-    }
-    ;
-    if (document.body.scrollTop >= 264 || document.documentElement.scrollTop >= 264) {
-        document.getElementById("reflection4").classList.add("animate");
-    }
-    ;
-};
