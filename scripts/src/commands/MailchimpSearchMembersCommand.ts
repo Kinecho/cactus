6f6d7dedc8db4d9d748d339c4266354111b90f7d
Neil Poulin <neil.j.poulin@gmail.com>
2019-06-27 15:15:24 -0600

    open file on computer

--- scripts/src/commands/MailchimpSearchMembersCommand.ts
+++ scripts/src/commands/MailchimpSearchMembersCommand.ts
@@ -6,6 +6,7 @@ import chalk from "chalk";
 import {writeToFile} from "@scripts/util/FileUtil";
 import helpers from "@scripts/helpers";
 const prompts = require("prompts");
+const exec = require("child_process").exec;
 
 
 export default class MailchimpSearchMembersCommand implements Command {
@@ -37,7 +38,17 @@ export default class MailchimpSearchMembersCommand implements Command {
         const data = JSON.stringify(searchResult, null, 2);
         await writeToFile(fileName, data);
         console.log(chalk.yellow(data));
-        console.log(chalk.green(`\nResults saved to ${fileName}`));
+        console.log(chalk.green(`\nResults saved to ${fileName}\n`));
+
+        const doOpenResponse:{doOpen:boolean} = await prompts({
+            type: "confirm",
+            message: "Do you want to open this file on your computer?",
+            name: "doOpen"
+        });
+
+        if (doOpenResponse.doOpen){
+            exec(`open ${fileName}`);
+        }
     }
 
 }
