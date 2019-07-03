ff2a6adc8fbb63a603e9fbf3ddaac025f620255c
Scott Rocher <scottrocher@gmail.com>
2019-07-03 12:25:59 -0700

    Looking forward to 2019-07-07

--- web/pages.js
+++ web/pages.js
@@ -282,6 +282,12 @@ module.exports = {
     "what_are_three_benefits_or_gifts_that_you_received_this_past_week_for_which_you_are_grateful_20190706": {
         "title": "What are three benefits or gifts that you received this past week for which you are grateful?",
         "path": "/what-are-three-benefits-or-gifts-that-you-received-this-past-week-for-which-you-are-grateful-20190706",
+        "reflectionPrompt": true,
+        "name": "what_are_three_benefits_or_gifts_that_you_received_this_past_week_for_which_you_are_grateful_20190706"
+    },
+    "what_three_things_are_you_looking_forward_to_experiencing_this_week_20190707": {
+        "title": "What three things are you looking forward to experiencing this week?",
+        "path": "/what-three-things-are-you-looking-forward-to-experiencing-this-week-20190707",
         "reflectionPrompt": true
     }
 }
