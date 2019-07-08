e0596a34ae3221b75567e75ecc6b930b77f9c397
Neil Poulin <neil.j.poulin@gmail.com>
2019-07-08 02:06:45 -0600

    get backup jobs for both analytics & firestore working

--- functions/src/endpoints/DataExportJob.ts
+++ functions/src/endpoints/DataExportJob.ts
@@ -2,10 +2,12 @@ import {getConfig} from "@api/config/configService";
 import * as functions from "firebase-functions";
 import {JWT} from "google-auth-library";
 import axios, {AxiosError} from "axios";
-import {sendEngineeringMessage} from "@api/slack/slack";
+import {AttachmentColor, sendEngineeringMessage, SlackAttachment} from "@api/slack/slack";
 import AdminFirestoreService from "@shared/services/AdminFirestoreService";
 import {Operation} from "@shared/types/FirestoreTypes";
 import {formatDuration} from "@shared/util/DateUtil";
+import {Storage} from "@google-cloud/storage";
+import {writeToFile} from "@api/util/FileUtil";
 
 const Config = getConfig();
 
@@ -21,15 +23,147 @@ const defaultScopes = [
     "https://www.googleapis.com/auth/cloud-platform",
 ];
 
-export async function backupFirestore(message?: functions.pubsub.Message, context?: functions.EventContext): Promise<void> {
+export const prefixFilename = "latest-prefix.txt";
 
+export async function backupFirestore(message?: functions.pubsub.Message, context?: functions.EventContext): Promise<void> {
     const collectionIds = await getCollectionIds();
     console.log("collectionIds", collectionIds);
-    await sendEngineeringMessage(`Backing up collections:\n \`\`\`${collectionIds.join(", ")}\`\`\`\nsaving to bucket: \`${backupsConfig.firestore_backups_bucket}\``);
 
-    await initializeBackup(serviceAccount.project_id, backupsConfig.firestore_backups_bucket, collectionIds)
+    const startTime = new Date();
+    await sendEngineeringMessage({
+        text: `Starting database backup job.`,
+        attachments: [
+            {
+                text: `Collection IDs:\n \`\`\`${collectionIds.join(", ")}\`\`\``,
+                color: AttachmentColor.info,
+                ts: `${(new Date()).getTime() / 1000}`
+            }
+        ]
+    });
+
+    try {
+        const [
+            exportOperation,
+            bigQueryOperation
+        ] = await Promise.all([
+            exportForBackup(),
+            exportForBigQuery(collectionIds)
+        ]);
+
+        const endTime = new Date();
+
+        const attachments: SlackAttachment[] = [
+            buildOperationAttachment("Firestore Backup", exportOperation.operation, formatDuration(startTime, exportOperation.endTime), exportOperation.error),
+            buildOperationAttachment("Big Query Export", bigQueryOperation.operation, formatDuration(startTime, bigQueryOperation.endTime), bigQueryOperation.error),
+        ];
+
+        await sendEngineeringMessage({
+            text: `Export Completed after ${formatDuration(startTime, endTime)}`,
+            attachments,
+        });
+    } catch (e) {
+        console.error("failed to backup", e);
+        await sendEngineeringMessage(`Failed to backup ${JSON.stringify(e, null, 2)}`);
+    }
+
+
 }
 
+function buildOperationAttachment(displayName: string, operation?: Operation, duration?: string, error?: any): SlackAttachment {
+    if (error) {
+        return {
+            text: `*${displayName}* error occurred ${duration ? `after ${duration}` : ""}\n\`\`\`${JSON.stringify(error, null, 2)}\`\`\``,
+            color: "danger",
+            ts: `${(new Date()).getTime() / 1000}`
+        }
+    }
+
+    if (!operation) {
+        return {
+            text: `*${displayName}* Unable to get completed operation `,
+            color: "danger",
+            ts: `${(new Date()).getTime() / 1000}`
+        }
+    }
+
+    let webLink: string | undefined = undefined;
+    let outputUriPrefix: string | undefined = undefined;
+    if (operation.metadata) {
+        outputUriPrefix = operation.metadata.outputUriPrefix;
+    }
+    let linkString = "";
+    if (outputUriPrefix) {
+        const bucketPath = outputUriPrefix.replace("gs://", "");
+        webLink = `https://console.cloud.google.com/storage/browser/${bucketPath}?project=${serviceAccount.project_id}`;
+        linkString = `<${webLink}|View Bucket>`
+    }
+
+    return {
+        text: `*${displayName}* completed successfully after ${duration}. ${linkString}\nOperation details\n\`\`\`${JSON.stringify(operation, null, 2)}\`\`\``,
+        color: "good",
+        ts: `${(new Date()).getTime() / 1000}`
+    }
+}
+
+// @ts-ignore
+async function exportForBackup(): Promise<{ operation?: Operation, endTime: Date, error?: any }> {
+
+    const {operation, error} = await initializeBackup(serviceAccount.project_id, backupsConfig.firestore_backups_bucket);
+    if (!operation) {
+        return {endTime: new Date(), error: error || {message: "Unable to initialize operation"}};
+    }
+
+    try {
+        const finishedOperation = await pollForOperation(operation.name);
+        const endTime = new Date();
+        return {operation: finishedOperation, endTime};
+
+    } catch (e) {
+        const endTime = new Date();
+        return {endTime, error: e};
+    }
+}
+
+async function exportForBigQuery(collectionIds: string[]): Promise<{ operation?: Operation, endTime: Date, error?: any }> {
+    // const operation = await initializeBackup(serviceAccount.project_id, backupsConfig.bigquery_import_bucket, collectionIds);
+    const analyticsProjectId = backupsConfig.analytics_project_id;
+
+    console.log("got bucket");
+    // firebase.storage().bucket
+
+    const date = new Date();
+    const nextPrefix = date.toISOString();
+    try {
+        const storage = new Storage({projectId: analyticsProjectId, credentials: serviceAccount});
+        const bucket = storage.bucket(backupsConfig.bigquery_import_bucket);
+        const tmpFilePath = `/tmp/${prefixFilename}`;
+        await writeToFile(tmpFilePath, nextPrefix);
+        await bucket.upload(tmpFilePath);
+    } catch (error) {
+        console.error("Failed to upload new prefix for bigquery export file to storage", error)
+        return {endTime: new Date(), error}
+    }
+
+    const {operation, error: initError} = await initializeBackup(serviceAccount.project_id,
+        `${backupsConfig.bigquery_import_bucket}/${nextPrefix}`,
+        collectionIds);
+
+    if (!operation) {
+        return {endTime: new Date(), error: initError || {message: "Failed to initialize bigqury export operation"}}
+    }
+
+
+    try {
+        const finishedOperation = await pollForOperation(operation.name);
+        const endTime = new Date();
+        return {operation: finishedOperation, endTime};
+
+    } catch (e) {
+        const endTime = new Date();
+        return {endTime, error: e};
+    }
+
+}
 
 export async function getOperation(name: string): Promise<Operation | undefined> {
     const uri = `https://firestore.googleapis.com/v1/${name}`;
@@ -140,10 +274,9 @@ async function getAuthHeaders(): Promise<{ [key: string]: string }> {
  * @param {Array<String>} [collectionIds] - optional list of collection ids to export. If undefined, it will export all collections
  * @returns {Promise<*>}
  */
-async function initializeBackup(projectId: string, outputBucket: string, collectionIds?: string[]) {
+async function initializeBackup(projectId: string, outputBucket: string, collectionIds?: string[]): Promise<{ operation?: Operation, error?: any }> {
     console.log("starting backup job for project", projectId);
     console.log("output bucket", outputBucket);
-    const startTime = new Date();
     const exportUrl = buildExportUrl(projectId);
     console.log("got access token and exportUrl = ", exportUrl);
     try {
@@ -160,56 +293,10 @@ async function initializeBackup(projectId: string, outputBucket: string, collect
         const operation = response.data as Operation;
 
         console.log("backup response data", operation);
-        let webLink: string | undefined = undefined;
-        let outputUriPrefix: string | undefined = undefined;
-        if (operation && operation.metadata) {
-            outputUriPrefix = operation.metadata.outputUriPrefix;
-        }
-        let linkString = "";
-        if (outputUriPrefix) {
-            const bucketPath = outputUriPrefix.replace("gs://", "");
-            webLink = `https://console.cloud.google.com/storage/browser/${bucketPath}?project=${projectId}`;
-            linkString = `<${webLink}|${outputUriPrefix}>`
-        }
-        await sendEngineeringMessage({
-            text: `Successfully initialized backup ${linkString ? "to " + linkString : ""}`,
-            attachments: [{text: `\`\`\`${JSON.stringify(response.data, null, 2)}\`\`\``, color: "good"}]
-        });
-
-
-        try {
-            const finishedOperation = await pollForOperation(operation.name);
-            const endTime = new Date();
-
-            if (finishedOperation) {
-                await sendEngineeringMessage({
-                    text: `:white_check_mark: Completed firestore backup operation after ${formatDuration(startTime, endTime)}`,
-                    attachments: [
-                        {
-                            text: `Operation details\n\`\`\`${JSON.stringify(finishedOperation, null, 2)}\`\`\``,
-                            color: "good",
-                            ts: `${(new Date()).getTime() / 1000}`
-                        }
-                    ]
-                })
-            }
-        } catch (e) {
-            const endTime = new Date();
-            await sendEngineeringMessage({
-                text: `Failed to find completed firestore backup operation after ${formatDuration(startTime, endTime)}`,
-                attachments: [{
-                    text: `Operation Name ${operation.name}\n\`\`\`${JSON.stringify(e, null, 2)}\`\`\``,
-                    color: "danger",
-                    ts: `${(new Date()).getTime() / 1000}`
-                }]
-            })
-        }
+        return {operation};
 
     } catch (error) {
         console.error("failed to initialize backup", error);
-        await sendEngineeringMessage({
-            text: "Failed to initialize backup",
-            attachments: [{text: `\`\`\`${JSON.stringify(error, null, 2)}\`\`\``, color: "danger", ts: `${(new Date()).getTime() / 1000}`}]
-        });
+        return {error};
     }
 }
