c8ff75cb6667a9e4c1bb4a30ed4e7c211c64f31e
Neil Poulin <neil.j.poulin@gmail.com>
2019-07-08 15:17:52 -0600

    remove bigquery and backup jobs from test app

--- functions/src/endpoints/testApp.ts
+++ functions/src/endpoints/testApp.ts
@@ -1,28 +1,13 @@
 import * as express from "express";
 import * as cors from "cors";
 import {
-    backupFirestore,
     getOperation,
-    exportFirestoreToBigQuery
 } from "@api/endpoints/DataExportJob";
 
 const app = express();
 app.use(cors({origin: true}));
 app.get('/', (req, res) => {
     res.status(200).json({status: 'ok', queryParams: req.query});
-});
-
-app.get("/backups", async (req, res) => {
-    await backupFirestore();
-    res.send("done");
-});
-
-app.get("/bigquery", async (req, res) => {
-    const results = await exportFirestoreToBigQuery();
-    res.send({
-        status: "done",
-        results,
-    });
 });
 
 app.get("/operation", async (req, res) => {
