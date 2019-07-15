5c763d78e18b99e9d5c9b3d42beda0c08e9e8410
Katie Blackman <katie@kinecho.com>
2019-07-15 17:04:22 -0500

    what are three benefits…

--- web/pages.js
+++ web/pages.js
@@ -386,7 +386,14 @@ module.exports = {
         "title": "What are three benefits or gifts that you received this past week for which you are grateful?",
         "path": "/what-are-three-benefits-or-gifts-that-you-received-this-past-week-for-which-you-are-grateful-20190720",
         "reflectionPrompt": true,
-        "includeInDev": true,
+        "includeInDev": false,
         "name": "what_are_three_benefits_or_gifts_that_you_received_this_past_week_for_which_you_are_grateful_20190720"
+    },
+    "what_three_things_are_you_looking_forward_to_experiencing_this_week_20190721": {
+        "title": "What three things are you looking forward to experiencing this week?",
+        "path": "/what-three-things-are-you-looking-forward-to-experiencing-this-week-20190721",
+        "reflectionPrompt": true,
+        "includeInDev": true,
+        "name": "what_three_things_are_you_looking_forward_to_experiencing_this_week_20190721"
     }
 }
---
+++ web/src/scripts/pages/what_three_things_are_you_looking_forward_to_experiencing_this_week_20190721.ts
@@ -0,0 +1,9 @@
+// tslint:disable-next-line:no-implicit-dependencies
+import "@styles/pages/what_three_things_are_you_looking_forward_to_experiencing_this_week_20190721.scss"
+import {initializeArticlePage} from "@web/articleCommon";
+
+initializeArticlePage();
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("what_three_things_are_you_looking_forward_to_experiencing_this_week_20190721 loaded");
+});
