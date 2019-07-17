1a10e6a7ad5ae7b968875b007b57b2f264e21a80
Neil Poulin <neil.j.poulin@gmail.com>
2019-07-17 16:39:29 -0600

    update script to get username and email from git

--- scripts/src/postdeploy.ts
+++ scripts/src/postdeploy.ts
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import {getCactusConfig, Project} from "@scripts/config";
 import AdminSlackService, {ChatMessage} from "@shared/services/AdminSlackService";
 
+const git = require("simple-git")();
 
 (async () => {
     const projectId = process.env.GCLOUD_PROJECT;
@@ -26,10 +27,41 @@ import AdminSlackService, {ChatMessage} from "@shared/services/AdminSlackService
 
     AdminSlackService.initialize(config);
 
+
+    const {username} = await new Promise<{ username?: string }>(resolve => {
+        git.raw(['config', '--global', 'user.name'], (error: any, result: string) => {
+            if (error) {
+                console.error("error");
+            }
+            resolve({username: result});
+        });
+    });
+    const {email} = await new Promise<{ email?: string }>(resolve => {
+        git.raw(['config', '--global', 'user.email'], (error: any, result: string) => {
+            if (error) {
+                console.error("error");
+            }
+            resolve({email: result});
+        });
+    });
+    // const isRepo = await
+
+    let byLine = "unknown";
+    if (username && email) {
+        byLine = `${username.trim()} \<${email.trim()}\>`;
+    } else if (username) {
+        byLine = username.trim();
+    } else if (email) {
+        byLine = email.trim();
+    }
+
+    console.log(`username ${username} | email ${email}`);
+
     const message: ChatMessage = {
         text: "",
         attachments: [{
             title: `Finished deploying ${resourceName} :tada:`,
+            text: `_started by ${byLine}_`,
             color: "good",
             ts: `${(new Date()).getTime() / 1000}`
         }],
