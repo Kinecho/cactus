fcfc1567c954cdf9ddba021d15d91dc9cf866849
Neil Poulin <neil.j.poulin@gmail.com>
2019-07-19 09:47:17 -0600

    use promises in webpack config, and get the currnet git hash to set in sentry config

--- web/webpack/webpack.config.fe.js
+++ web/webpack/webpack.config.fe.js
@@ -1,9 +1,14 @@
-const merge = require("webpack-merge");
-const getCommonWebpackConfig = require("./webpack.config.common")
-const getDevServerConfig = require("./devserver.config")
+const merge = require('webpack-merge')
+const getCommonWebpackConfig = require('./webpack.config.common')
+const getDevServerConfig = require('./devserver.config')
 const config = require('./config.fe')
 
-module.exports = merge(getCommonWebpackConfig(config), {
-    mode: "development",
-    devtool: "cheap-module-eval-source-map",
-}, getDevServerConfig(config))
+module.exports = () => {
+    return getCommonWebpackConfig(config).then(common => {
+        return merge(common, {
+            mode: 'development',
+            devtool: 'cheap-module-eval-source-map',
+        }, getDevServerConfig(config))
+    })
+}
+
