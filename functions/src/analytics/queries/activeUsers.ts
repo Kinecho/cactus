c85cf23285211842893db15b681917f1478e21f3
Scott Rocher <scottrocher@gmail.com>
2019-07-20 14:06:14 -0700

    Simplify and fix logic after testing on BigQuery
    Use TIMESTAMP_SUB instead of TIME_SUB

--- functions/src/analytics/queries/activeUsers.ts
+++ functions/src/analytics/queries/activeUsers.ts
@@ -3,11 +3,9 @@ export interface ActiveUsersArguments {
 }
 
 export function buildActiveUsersQuery(args: ActiveUsersArguments): string {
-    const hours = args.days * 24;
-    
     return `select count(distinct cactusMemberId) as count
 from cactus_prod.reflection_responses r
-where r.createdAt > TIME_SUB(CURRENT_TIMESTAMP(), INTERVAL ${hours} HOUR)`;
+where r.createdAt > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL ${args.days} DAY)`;
 }
 
 export function processActiveUsersResults(rows: any[]): number {
