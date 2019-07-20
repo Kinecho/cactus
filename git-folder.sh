6a214d20711a57b02b88fb3999496c63f5ca7196
Neil Poulin <neil.j.poulin@gmail.com>
2019-07-20 12:37:07 -0600

    clean up the git script

--- git-folder.sh
+++ git-folder.sh
@@ -10,26 +10,23 @@ echo "======== CHANGE LIST ========="
 echo "${LATEST_COMMIT}"
 git log -m -1 --pretty=%B
 git diff-tree --no-commit-id -m --name-only -r "$LATEST_COMMIT"
+
+
+
 echo ""
-
-# latest commit where path/to/folder1 was changed
-WEB_FOLDER_COMMIT=$(git log -m -1 --name-only --format=format:%H  web)
-#git diff-tree --no-commit-id -m --name-only -r "$LATEST_COMMIT" web && HAS_WEB_CHANGES=true || HAS_WEB_CHANGES=false
-
 echo "============ Web ============="
-WEB_FILES=$(git diff-tree --no-commit-id -m --name-only -r "$LATEST_COMMIT" web )
+WEB_FILES=$(git diff-tree --no-commit-id -m --name-only -r "$LATEST_COMMIT" web | xargs)
 
 HAS_WEB_CHANGES=false
-if [[ WEB_FILES ]]; then
+if [[ ${WEB_FILES} ]]; then
     echo "${WEB_FILES}"
     HAS_WEB_CHANGES=true
 else
     echo "No Changes"
 fi
+
+
 echo ""
-
-API_FOLDER_COMMIT=$(git log -m -1 --name-only --format=format:%H --full-diff functions)
-
 echo "============ API ============="
 API_FILES=$(git diff-tree --no-commit-id -m --name-only -r "$LATEST_COMMIT" functions | xargs)
 
@@ -42,8 +39,6 @@ else
 fi
 
 echo ""
-
-
 echo "=========== SHARED ==========="
 SHARED_FILES=$(git diff-tree --no-commit-id -m --name-only -r "$LATEST_COMMIT" shared | xargs)
 
@@ -54,10 +49,8 @@ if [[ ${SHARED_FILES} ]]; then
 else
     echo "No changes"
 fi
+
 echo ""
-
-
-
 echo "=========== SCRIPTS ==========="
 SCRIPTS_FILES=$(git diff-tree --no-commit-id -m --name-only -r "$LATEST_COMMIT" scripts | xargs)
 
@@ -85,26 +78,34 @@ FIREBASE_COMMANDS=()
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 
-if [[ ${HAS_WEB_CHANGES} || ${HAS_SHARED_CHANGES} ]]; then
+if ${HAS_WEB_CHANGES} || ${HAS_SHARED_CHANGES}; then
         echo "Adding hosting to deploy list"
         FIREBASE_COMMANDS+=('hosting')
 fi
 
-if [[ ${HAS_API_CHANGES} || ${HAS_SHARED_CHANGES} ]]; then
+if ${HAS_API_CHANGES} || ${HAS_SHARED_CHANGES}; then
         echo "Adding functions to deploy list"
         FIREBASE_COMMANDS+=('functions')
 fi
 
-if [[ ${HAS_SHARED_CHANGES} ]]; then
+if  ${HAS_SHARED_CHANGES} ; then
         echo "files in SHARED has changed - not doing anything special"
 fi
 
-if [[ ${HAS_SCRIPTS_CHANGES} ]]; then
+if ${HAS_SCRIPTS_CHANGES} ; then
         echo "files in SCRIPTS has changed - need to run tests"
+fi
+
+if ! [[ ${HAS_WEB_CHANGES} && ${HAS_API_CHANGES} && ${HAS_SHARED_CHANGES} && ${HAS_SCRIPTS_CHANGES} ]]; then
+    echo "NO changes at all. "
 fi
 
 
 FIREBASE_MODULES=$(join_by , ${FIREBASE_COMMANDS[@]})
 
 echo
-echo "Will execute firebase command \"firebase deploy --only ${FIREBASE_MODULES}\""
+if [[ $(${FIREBASE_MODULES} | xargs) ]]; then
+    echo "Will execute firebase command \"firebase deploy --only ${FIREBASE_MODULES}\""
+else
+    echo "Nothing to deploy"
+fi
