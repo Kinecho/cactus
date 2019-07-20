763737a6cf60a54b022dc52e018a5dd81a43306a
Neil Poulin <neil.j.poulin@gmail.com>
2019-07-20 11:30:13 -0600

    update ci script

--- git-folder.sh
+++ git-folder.sh
@@ -5,7 +5,12 @@
 # latest commit
 LATEST_COMMIT=$(git rev-parse HEAD)
 
-echo "LATEST_COMMIT = ${LATEST_COMMIT}: $(git log -1 --pretty=%B)"
+echo "LATEST_COMMIT = ${LATEST_COMMIT}: $(git log -m -1 --pretty=%B)"
+echo ""
+echo "=====Changed files===="
+git diff-tree --no-commit-id --name-only -r "$LATEST_COMMIT"
+echo "======================"
+echo ""
 
 # latest commit where path/to/folder1 was changed
 WEB_FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff web)
@@ -15,19 +20,35 @@ API_FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff functions)
 
 SHARED_FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff shared)
 
-if [[ ${WEB_FOLDER_COMMIT} = ${LATEST_COMMIT} ]];
+SCRIPTS_FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff scripts)
+
+FIREBASE_COMMANDS=()
+
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+
+if [[ ${SHARED_FOLDER_COMMIT} = ${LATEST_COMMIT} ]] || [[ ${WEB_FOLDER_COMMIT} = ${LATEST_COMMIT} ]];
     then
-        echo "files in WEB has changed"
-#        .circleci/do_something.sh
-elif [[ ${API_FOLDER_COMMIT} = ${LATEST_COMMIT} ]];
-    then
-        echo "files in FUNCTIONS has changed"
-#        .circleci/do_something_else.sh
-elif [[ ${SHARED_FOLDER_COMMIT} = ${LATEST_COMMIT} ]];
-    then
-        echo "files in SHARED has changed"
-#        .circleci/do_something_else.sh
-else
-     echo "no folders of relevance has changed"
-#     exit 0;
+        echo "Commit has web or shared, adding hosting to deploy list"
+        FIREBASE_COMMANDS+=('hosting')
 fi
+
+if [[ ${SHARED_FOLDER_COMMIT} = ${LATEST_COMMIT} ]] || [[ ${API_FOLDER_COMMIT} = ${LATEST_COMMIT} ]];
+    then
+        echo "Commit has functions or shared, adding functions to deploy list"
+        FIREBASE_COMMANDS+=('functions')
+fi
+
+
+if [[ ${SHARED_FOLDER_COMMIT} = ${LATEST_COMMIT} ]];
+    then
+        echo "files in SHARED has changed - not doing anything special"
+fi
+
+if [[ ${SCRIPTS_FOLDER_COMMIT} = ${LATEST_COMMIT} ]];
+    then
+        echo "files in SCRIPTS has changed - need to run tests"
+fi
+
+join_by , ${FIREBASE_COMMANDS[@]}
