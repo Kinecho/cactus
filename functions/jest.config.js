a6bc24fa47bfebc43f7aaa40598216511bea92b0
Neil Poulin <neil.j.poulin@gmail.com>
2019-07-21 13:27:26 -0600

    add jest config for functions

---
+++ functions/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    roots: [
+        "<rootDir>/src"
+    ],
+    preset: "ts-jest",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    moduleNameMapper: {
+        '^@api/(.*)$': '<rootDir>/src/$1',
+        '^@shared/(.*)$': '<rootDir>/../shared/src/$1',
+    }
+}
