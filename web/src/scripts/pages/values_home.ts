ec35351cee9c9b17741d8256017b843bafaf6645
Neil Poulin <neil.j.poulin@gmail.com>
2019-07-29 18:54:01 -0600

    add values home placeholders

---
+++ web/src/scripts/pages/values_home.ts
@@ -0,0 +1,9 @@
+// tslint:disable-next-line:no-implicit-dependencies
+import "@styles/pages/values_home.scss"
+import {initializeArticlePage} from "@web/articleCommon";
+
+initializeArticlePage();
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("values_home loaded");
+});
