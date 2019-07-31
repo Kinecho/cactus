f2c75ebb94dd03d68f16f168277fb9c1135dac45
Katie Blackman <katie@kinecho.com>
2019-07-31 12:27:17 -0600

    overdue initial commit of values page, good on desktop, animations and mobile next

---
+++ web/src/scripts/pages/values.ts
@@ -0,0 +1,93 @@
+// tslint:disable-next-line:no-implicit-dependencies
+import "@styles/pages/values.scss"
+// @ts-ignore
+import * as ScrollMagic from "scrollmagic";
+import {TweenMax, Power1} from "gsap";
+import 'animation.gsap';
+// @ts-ignore
+import {ScrollToPlugin} from 'gsap/all';
+
+const controller = new ScrollMagic.Controller();
+
+//need to reference the plugin here so it doesn't get dropped by webpack during tree shaking
+const plugins = [ScrollToPlugin, TweenMax, Power1];
+console.debug("using plugins", plugins);
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("values loaded");
+
+    configureLearnMoreScroll();
+    configureAnimations();
+});
+
+function configureLearnMoreScroll() {
+
+    controller.scrollTo((newpos: any) => {
+        const width = window.innerWidth;
+
+        let duration = 1.0;
+        let autoKill = true;
+        if (width < 1140) {
+            duration = 0.25;
+            autoKill = false;
+        }
+
+        TweenMax.to(window, duration, {scrollTo: {y: newpos, autoKill}, ease: Power1.easeIn});
+    });
+
+    const learnMore = document.getElementById("learnMoreTop");
+    if (learnMore) {
+        learnMore.addEventListener("click", (e) => {
+            e.preventDefault();
+            controller.scrollTo("#learnMore");
+        })
+    }
+
+}
+
+function configureAnimations() {
+
+    const width = window.innerWidth;
+
+    if (width >= 1140) {
+        new ScrollMagic.Scene({
+            offset: 1,
+            duration: '100%',
+        }).setTween(".reflection", 1, {transform: 'translate(0,0)'})
+            .addTo(controller);
+    }
+
+    if (width >= 1140) {
+        new ScrollMagic.Scene({
+            offset: 1,
+            duration: '100%',
+        }).setTween("#pinkBlob", 1, {transform: 'translate(81vw, 140vh)'})
+            .addTo(controller);
+    } else if (width < 1140) {
+        new ScrollMagic.Scene({
+            offset: 1,
+            duration: '100%',
+        }).setTween("#pinkBlob", 1, {transform: 'translate(81vw, 60vh)'})
+            .addTo(controller);
+    }
+
+    new ScrollMagic.Scene({
+        offset: 1,
+        duration: '500%',
+    }).setTween("#yellowBlob1", 1, {transform: 'translate(-7vw, 99vh) rotate(15deg)'})
+        .addTo(controller);
+
+    if (width >= 1140) {
+        new ScrollMagic.Scene({
+            offset: 1,
+            duration: '100%',
+        }).setTween("#greenBlob", 1, {transform: 'translate(-12vw, 110vh)'})
+            .addTo(controller);
+    } else if (width < 1140) {
+        new ScrollMagic.Scene({
+            offset: 1,
+            duration: '100%',
+        }).setTween("#greenBlob", 1, {transform: 'translate(-24vw, 65vh)'})
+            .addTo(controller);
+    }
+}
