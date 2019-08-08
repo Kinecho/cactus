48961f2b26b7c4a23f151e1a0c38868f4b8eb523
Scott Rocher <scottrocher@gmail.com>
2019-08-08 13:59:40 -0700

    No longer need to import SMS icon

--- web/src/scripts/pages/referral_program.ts
+++ web/src/scripts/pages/referral_program.ts
@@ -5,7 +5,7 @@ import VueClipboard from 'vue-clipboard2';
 import SocialSharing from 'vue-social-sharing';
 import ReferralProgram from "@components/ReferralProgram.vue";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope, faSms } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
