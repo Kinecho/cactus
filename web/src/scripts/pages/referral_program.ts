fa7b795aa3ae80463f0e7999304ec51ed0e9f035
Scott Rocher <scottrocher@gmail.com>
2019-08-12 12:16:27 -0700

    Remove fontawesome, fix clipboard copy
    @katieb Replaced the fontawesome icons with custom ones so we no longer need the fontawesome component.
    
    Also the vue-clipboard2 and vue-social-sharing modules were missing from the package files.

--- web/src/scripts/pages/referral_program.ts
+++ web/src/scripts/pages/referral_program.ts
@@ -4,19 +4,9 @@ import Vue from "vue";
 import VueClipboard from 'vue-clipboard2';
 import SocialSharing from 'vue-social-sharing';
 import ReferralProgram from "@components/ReferralProgram.vue";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 Vue.use(VueClipboard);
 Vue.use(SocialSharing);
-
-library.add(faEnvelope);
-library.add(faTwitter, faFacebook);
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.config.productionTip = false
 
 new Vue({
     el: "#app",
