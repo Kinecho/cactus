02440e516d90033970a35ae208569defb34dc0e0
Neil Poulin <neil.j.poulin@gmail.com>
2019-08-12 23:31:39 -0600

    make the modal mask more opaque

--- web/src/scripts/components/Modal.vue
+++ web/src/scripts/components/Modal.vue
@@ -62,7 +62,7 @@
         left: 0;
         width: 100%;
         height: 100%;
-        background-color: rgba(255, 255, 255, .8);
+        background-color: rgba(255, 255, 255, .9);
         transition: opacity .3s ease;
         overflow-y: auto;
     }
