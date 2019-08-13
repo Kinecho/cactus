3620d17f3577e3d9d02b9e63e19ce99971b3e9a3
Neil Poulin <neil.j.poulin@gmail.com>
2019-08-12 21:29:18 -0600

    add additonal offset prop

--- web/src/scripts/components/ResizableTextarea.vue
+++ web/src/scripts/components/ResizableTextarea.vue
@@ -5,36 +5,23 @@
                 type: Number,
                 default: 100,
             },
+            additionalOffsetPx: {
+                type: Number,
+                default: 2,
+            }
         },
         methods: {
             resizeTextarea(event) {
-
-
-                // const currentHeight = event.target.scrollHeight
-                // const lht = parseInt(event.target.lineHeight, 10)
-                // // const lines =currentHeight / lht;
-                // const maxHeight = lht * this.maxLines
-                // console.log('line height', lht)
-                // console.log('maxHeight ', maxHeight)
-                // console.log('current height', currentHeight)
-                // if (currentHeight < maxHeight) {
-                //
-                //     event.target.style.height = 'auto'
-                //     const newHeight = Math.min(maxHeight, event.target.scrollHeight)
-                //     event.target.style.height = (newHeight) + 'px'
-                // }
-
-
                 console.log('number of rows', event.target.rows)
                 event.target.style.height = 'auto'
-                const newHeight = event.target.scrollHeight + 2
+                const newHeight = event.target.scrollHeight + this.additionalOffsetPx //adding 2 to combat the weird scrolling when it is initially rendered
                 event.target.style.height = (Math.min(this.maxHeightPx, newHeight)) + 'px'
 
             },
         },
         mounted() {
             this.$nextTick(() => {
-                this.$el.setAttribute('style', 'height:' + (this.$el.scrollHeight + 2) + 'px;overflow-y:auto;')
+                this.$el.setAttribute('style', 'height:' + (this.$el.scrollHeight + this.additionalOffsetPx) + 'px;overflow-y:auto;')
             })
 
             this.$el.addEventListener('input', this.resizeTextarea)
