1d53aff942dcdab64fe6afb43d651e35de9499b3
Neil Poulin <neil.j.poulin@gmail.com>
2019-08-22 22:09:25 -0600

    add auth test for query params [ci skip]

--- web/src/scripts/util.test.ts
+++ web/src/scripts/util.test.ts
@@ -1,3 +1,40 @@
-test("placeholder", () => {
-    expect(true).toBeTruthy();
+import {getQueryParam} from "@web/util";
+import {QueryParam} from "@shared/util/queryParams";
+
+
+const jest = require("jest");
+import {mockFirebase, mockauth} from "@test/jestsetup";
+
+jest.mock("@web/auth", () => {
+    return {
+        logout: () => undefined,
+    }
 });
+
+jest.mock("@web/firebase", () => {
+    return {
+        getAuth: () => mockauth,
+        initializeFirebase: () => mockFirebase
+    };
+});
+
+describe("getQueryParam", () => {
+    test("getQueryParam - not encoded properly ", () => {
+        const url = "/prompts/nIVmhvgppLNgNMAsWEb0?e=scottrocher+test082019@gmail.com";
+        window.history.pushState({}, 'Test Title', url);
+
+        expect(getQueryParam(QueryParam.EMAIL)).toEqual("scottrocher test082019@gmail.com")
+    });
+
+    test("getQueryParam - encoded query param", () => {
+        const url = "/prompts/nIVmhvgppLNgNMAsWEb0?e=scottrocher%2Btest082019%40gmail.com";
+        console.log("encodedComponent", url);
+        window.history.pushState({}, 'Test Title', url);
+
+        expect(getQueryParam(QueryParam.EMAIL)).toEqual("scottrocher+test082019@gmail.com")
+    });
+
+});
+
+
+
