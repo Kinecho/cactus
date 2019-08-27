eb0cae9a0b33a8ef6c83b12e0761db8a1732d80e
Scott Rocher <scottrocher@gmail.com>
2019-08-27 13:54:49 -0700

    Don't replace forward slashes with spaces
    I believe this was an bug based on the comment

--- shared/src/util/StringUtil.ts
+++ shared/src/util/StringUtil.ts
@@ -6,7 +6,7 @@ import ReflectionResponse from "@shared/models/ReflectionResponse";
 export function removeSpecialCharacters(input: string, replacement: string): string {
     return input.trim().toLowerCase()
         .replace(/[^a-z0-9-_\s\\\/]/g, "") //remove special characters
-        .replace(/[-_\\\/]/g, " ") //replace underscores or hyphens with space
+        .replace(/[-_]/g, " ") //replace underscores or hyphens with space
         .replace(/(\s+)/g, replacement); //replace all spaces with hyphen
 }
 
