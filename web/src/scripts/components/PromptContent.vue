1bd6bee807f4ab9cdd437fe6c01bc922f2df0ecb
Neil Poulin <neil.j.poulin@gmail.com>
2019-09-09 16:18:52 -0600

    don't enable tap anywhere on the share screen

--- web/src/scripts/components/PromptContent.vue
+++ web/src/scripts/components/PromptContent.vue
@@ -372,7 +372,7 @@
                 }
             },
             tapAnywhereEnabled(): boolean {
-                return true
+                return !this.isShareNote;
             },
         },
         watch: {
