c424805a5db77139aa9590459e6eff05822cc535
Katie Blackman <katie@cactus.app>
2019-10-15 11:20:37 -0600

    add element definitions to en-us

--- shared/src/copy/en-US.ts
+++ shared/src/copy/en-US.ts
@@ -57,6 +57,13 @@ export default class EnglishCopy extends LocalizedCopy {
         SHARE_NOTE: "Share Note",
         SHARE_PROMPT_COPY_MD: "Become more courageous and **build trust and connection** with others.",
     };
+    elements: ElementCopy = {
+        MEANING_DESCRIPTION: "Living with a sense of purpose while you enjoy the present",
+        EXPERIENCE_DESCRIPTION: "Exploring your intellectual curiosities and openly observing life’s lessons",
+        ENERGY_DESCRIPTION: "Caring for your physical health and managing the mind-body connection",
+        EMOTIONS_DESCRIPTION: "Embracing the spectrum of emotions as you strive for optimism",
+        RELATIONSHIPS_DESCRIPTION: "Developing rewarding and fulfilling relationships with yourself and others",
+    };
     error: ErrorCopy = {
         PLEASE_ENTER_A_VALID_EMAIL_ADDRESS: "Please enter a valid email address",
         PLEASE_ENTER_AN_EMAIL_ADDRESS: "Please enter an email address",
