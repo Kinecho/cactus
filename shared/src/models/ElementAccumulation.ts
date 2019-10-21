cc1e70eb66e8f35c85411eba6f408822658276aa
Scott Rocher <scottrocher@gmail.com>
2019-10-21 13:48:10 -0700

    Fix linting error, switch to type

--- shared/src/models/ElementAccumulation.ts
+++ shared/src/models/ElementAccumulation.ts
@@ -1,6 +1,6 @@
 import {BaseModel, Collection} from "@shared/FirestoreBaseModels";
 import {CactusElement} from "@shared/models/CactusElement";
 
-export interface ElementAccumulation {
-  [key: keyof typeof CactusElement]: number
+export type ElementAccumulation = {
+  [key in CactusElement]: number;
 };
