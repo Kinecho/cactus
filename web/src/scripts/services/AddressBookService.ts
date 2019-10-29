ce42707c64759506bf2e7d5cb2138af29e08ade9
Scott Rocher <scottrocher@gmail.com>
2019-10-28 17:34:24 -0700

    Add simple test integration

--- web/src/scripts/services/AddressBookService.ts
+++ web/src/scripts/services/AddressBookService.ts
@@ -3,7 +3,7 @@ import {Config} from "@web/config";
 class AddressBookService {
     static sharedInstance =new AddressBookService();
 
-    constructor() {
+    start() {
       let cloudSpongeScript = document.createElement('script');
           cloudSpongeScript.setAttribute('src', 'https://api.cloudsponge.com/widget/' + Config.cloudSpongeKey + '.js');
           document.head.appendChild(cloudSpongeScript);
