b33c8f7d510026b7ce11159b69b83dccac754bf0
Katie Blackman <katie@cactus.app>
2019-10-28 22:17:49 -0600

    stubs for imported list of contacts

--- web/src/scripts/components/SocialActivity.vue
+++ web/src/scripts/components/SocialActivity.vue
@@ -33,12 +33,72 @@
                     <h1>Find Your Friends</h1>
                     <p class="subtext">Invite people you know to join and reflect with you. Your reflections are always private unless you specifically share them.</p>
                     <button class="primary">Share your Invite Link</button>
-                    <h2>Import Your Contacts</h2>
+
+                    <!-- if not imported -->
+                    <!-- <h2>Import Your Contacts</h2>
                     <p class="subtext">Import your contacts from email. Don't worry, you'll choose who to connect with before they're invited.</p>
                     <div class="btnContainer">
                         <button class="secondary small">Gmail</button>
                         <button class="secondary small">Yahoo</button>
+                    </div> -->
+                    <!-- end -->
+
+                    <h2>Import Your Contacts <span class="resultCount">(123)</span></h2>
+                    <div class="contactCard">
+                        <div class="avatar">
+                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
+                        </div>
+                        <div class="contactInfo">
+                            <p class="name">James Brown</p>
+                            <p class="email">james@brown.com</p>
+                        </div>
+                        <button class="secondary small">Add Friend</button>
                     </div>
+                    <div class="contactCard">
+                        <div class="avatar">
+                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
+                        </div>
+                        <div class="contactInfo">
+                            <p class="name">James Brown</p>
+                            <p class="email">james@brown.com</p>
+                        </div>
+                        <button class="secondary small">Requested</button>
+                    </div>
+                    <div class="contactCard">
+                        <div class="avatar">
+                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
+                        </div>
+                        <div class="contactInfo">
+                            <p class="name">James Brown</p>
+                            <p class="email">james@brown.com</p>
+                        </div>
+                        <p class="friendsStatus">
+                            <svg class="check" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 13"><path fill="#FDBCA3" d="M1.707 6.293A1 1 0 0 0 .293 7.707l5 5a1 1 0 0 0 1.414 0l11-11A1 1 0 1 0 16.293.293L6 10.586 1.707 6.293z"/></svg>
+                            Friends
+                        </p>
+                    </div>
+                    <div class="contactCard">
+                        <div class="avatar">
+                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
+                        </div>
+                        <div class="contactInfo">
+                            <p class="name">James Brown</p>
+                            <p class="email">james@brown.com</p>
+                        </div>
+                        <button class="secondary small">Invite</button>
+                    </div>
+                    <div class="contactCard">
+                        <div class="avatar">
+                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
+                        </div>
+                        <div class="contactInfo">
+                            <p class="name">James Brown</p>
+                            <p class="email">james@brown.com</p>
+                        </div>
+                        <button class="secondary small">Invited</button>
+                    </div>
+
+
                 </div>
 
                 <!-- if has friends -->
@@ -243,6 +303,39 @@
         }
     }
 
+    .contactCard {
+        align-items: center;
+        display: flex;
+        max-width: 768px;
+        padding: 1.6rem 0;
+
+        .contactInfo {
+            flex-grow: 1;
+        }
+
+        .avatar {
+            $avatarDiameter: 4.4rem;
+            height: $avatarDiameter;
+            width: $avatarDiameter;
+        }
+
+        button {
+            flex-grow: 0;
+        }
+
+        .friendsStatus {
+            align-items: center;
+            color: $darkestPink;
+            display: flex;
+
+            .check {
+                height: 1.8rem;
+                margin-right: .8rem;
+                width: 1.8rem;
+            }
+        }
+    }
+
     .findFriends {
         margin: 0 auto 6.4rem;
         max-width: 960px;
@@ -303,26 +396,6 @@
             }
         }
 
-        .date {
-            font-size: 1.4rem;
-            opacity: .8;
-        }
-
-        .avatar {
-            $avatarDiameter: 6.4rem;
-            border-radius: 50%;
-            flex-shrink: 0;
-            height: $avatarDiameter;
-            margin-right: 1.6rem;
-            overflow: hidden;
-            width: $avatarDiameter;
-
-            img {
-                width: 100%;
-                height: 100%;
-            }
-        }
-
         a {
             text-decoration: none;
 
@@ -333,6 +406,27 @@
 
         .bold {
             font-weight: bold;
+        }
+    }
+
+    .email,
+    .date {
+        font-size: 1.4rem;
+        opacity: .8;
+    }
+
+    .avatar {
+        $avatarDiameter: 6.4rem;
+        border-radius: 50%;
+        flex-shrink: 0;
+        height: $avatarDiameter;
+        margin-right: 1.6rem;
+        overflow: hidden;
+        width: $avatarDiameter;
+
+        img {
+            width: 100%;
+            height: 100%;
         }
     }
 
