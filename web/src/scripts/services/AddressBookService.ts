602e1ce747aa500aa93fb8284114725b1dbb1860
Scott Rocher <scottrocher@gmail.com>
2019-10-29 11:25:39 -0700

    Show test area

--- web/src/scripts/services/AddressBookService.ts
+++ web/src/scripts/services/AddressBookService.ts
@@ -4,9 +4,12 @@ class AddressBookService {
     static sharedInstance =new AddressBookService();
 
     start() {
-      let cloudSpongeScript = document.createElement('script');
-          cloudSpongeScript.setAttribute('src', 'https://api.cloudsponge.com/widget/' + Config.cloudSpongeKey + '.js');
-          document.head.appendChild(cloudSpongeScript);
+      if (!document.getElementById('cloudsponge-' + Config.cloudSpongeKey)) {
+        let cloudSpongeScript = document.createElement('script');
+            cloudSpongeScript.setAttribute('id', 'cloudsponge-' + Config.cloudSpongeKey);
+            cloudSpongeScript.setAttribute('src', 'https://api.cloudsponge.com/widget/' + Config.cloudSpongeKey + '.js');
+            document.head.appendChild(cloudSpongeScript);
+      }
     }
 
 }
