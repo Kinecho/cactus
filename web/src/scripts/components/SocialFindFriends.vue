9ef32573971005f882f9b38f6858dec4c5532366
Scott Rocher <scottrocher@gmail.com>
2019-10-30 16:18:11 -0700

    WIP Move each contact to it's own component

--- web/src/scripts/components/SocialFindFriends.vue
+++ web/src/scripts/components/SocialFindFriends.vue
@@ -20,60 +20,7 @@
             <div class="results" v-if="importedContacts">
                 <h2>{{importedService}} Contacts <span class="resultCount">({{importedContacts.length}})</span></h2>
                 <div class="contactCards" v-for="contact in importedContacts">
-                    <div class="contactCard">
-                        <div class="avatar">
-                            <img :src="'https://i.pravatar.cc/44?u=' + contact.email" alt="User avatar"/>
-                        </div>
-                        <div class="contactInfo">
-                            <p class="name">{{contact.first_name}} {{contact.last_name}}</p>
-                            <p class="email">{{contact.email}}</p>
-                        </div>
-                        <button class="secondary small">Invite</button>
-                    </div>
-                    <!--
-                    <div class="contactCard">
-                        <div class="avatar">
-                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
-                        </div>
-                        <div class="contactInfo">
-                            <p class="name">James Brown</p>
-                            <p class="email">james@brown.com</p>
-                        </div>
-                        <button class="secondary small">Add Friend</button>
-                    </div>
-                    <div class="contactCard">
-                        <div class="avatar">
-                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
-                        </div>
-                        <div class="contactInfo">
-                            <p class="name">James Brown</p>
-                            <p class="email">james@brown.com</p>
-                        </div>
-                        <button class="secondary small">Requested</button>
-                    </div>
-                    <div class="contactCard">
-                        <div class="avatar">
-                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
-                        </div>
-                        <div class="contactInfo">
-                            <p class="name">James Brown</p>
-                            <p class="email">james@brown.com</p>
-                        </div>
-                        <p class="friendsStatus">
-                            <svg class="check" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 13"><path fill="#FDBCA3" d="M1.707 6.293A1 1 0 0 0 .293 7.707l5 5a1 1 0 0 0 1.414 0l11-11A1 1 0 1 0 16.293.293L6 10.586 1.707 6.293z"/></svg>
-                            Friends
-                        </p>
-                    </div>
-                    <div class="contactCard">
-                        <div class="avatar">
-                            <img src="https://placekitten.com/44/44" alt="User avatar"/>
-                        </div>
-                        <div class="contactInfo">
-                            <p class="name">James Brown</p>
-                            <p class="email">james@brown.com</p>
-                        </div>
-                        <button class="secondary small">Invited</button>
-                    </div>-->
+                    <SocialImportedContact v-bind:contact="contact"/>
                 </div>
             </div>
         </div>
@@ -83,11 +30,12 @@
 <script lang="ts">
     import Vue from "vue";
     import AddressBookService from '@web/services/AddressBookService'
+    import SocialImportedContact from "@components/SocialImportedContact.vue"
     import {EmailService} from "@shared/types/EmailContactTypes";
 
     export default Vue.extend({
         components: {
-
+            SocialImportedContact
         },
         created() {
             AddressBookService.sharedInstance.start();
