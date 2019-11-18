7cc236145b72150eb3b3baf8a4554358f07e123c
Scott Rocher <scottrocher@gmail.com>
2019-11-18 09:49:57 -0800

    Add SocialConnections to collection types

--- shared/src/FirestoreBaseModels.ts
+++ shared/src/FirestoreBaseModels.ts
@@ -11,7 +11,8 @@ export enum Collection {
     reflectionPrompt = "reflectionPrompt",
     sentPrompts = "sentPrompts",
     pendingUsers = "pendingUsers",
-    socialInvites = "socialInvites"
+    socialInvites = "socialInvites",
+    socialConnections = "socialConnections"
 }
 
 export interface FirestoreIdentifiable {
