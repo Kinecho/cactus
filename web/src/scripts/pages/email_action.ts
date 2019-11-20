9f29e5f8952934a68e91e6ce40c4d1a0e9928e01
Neil Poulin <neil.j.poulin@gmail.com>
2019-11-20 14:05:58 -0700

    update email action handler code

--- web/src/scripts/pages/email_action.ts
+++ web/src/scripts/pages/email_action.ts
@@ -1,5 +1,3 @@
-// tslint:disable-next-line:no-implicit-dependencies
-import "@styles/pages/email_action.scss"
 import Vue from "vue";
 import EmailActionHandler from "@components/EmailActionHandler.vue";
 
