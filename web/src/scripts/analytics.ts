852a39f3b81abe8abe125d5dd0fc424cd4cc1aa9
Scott Rocher <scottrocher@gmail.com>
2019-11-20 10:21:56 -0800

    Add Facebook pixel method

--- web/src/scripts/analytics.ts
+++ web/src/scripts/analytics.ts
@@ -46,6 +46,7 @@ let hasInit = false;
 export function init() {
     if (hasInit) {
         console.warn("Analytics already initialized, not reinitializing");
+        return;
     }
 
 
@@ -147,3 +148,15 @@ function createGTag() {
     }
     return _gtag
 }
+
+/* tslint: disable */
+// @ts-ignore:
+export function startFacebookPixel() {
+    // @ts-ignore:
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    // @ts-ignore:
+    fbq('init', '1422468787913495');
+    // @ts-ignore:
+    fbq('track', 'PageView');
+}
+/* tslint: enable */
