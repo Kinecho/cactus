e9266e9e2edfd3df8be46a6132db4b9ea94d9ccd
Scott Rocher <scottrocher@gmail.com>
2019-12-17 09:48:52 -0800

    Use promises to execute async in paralell

--- web/src/scripts/services/ImportedContactService.ts
+++ web/src/scripts/services/ImportedContactService.ts
@@ -10,26 +10,26 @@ class ImportedContactService {
   async prepareImportedContacts(emailContacts: EmailContact[],
                                 friendIds: string[], 
                                 requestedFriendIds: string[]): Promise<ImportedContact[]> {
-    const preparedContacts: ImportedContact[] = [];
+    
+    const preparedContacts:ImportedContact[] = await Promise.all(emailContacts.map(contact => {
+      return new Promise<ImportedContact>(async(resolve, reject) => {
+        const memberProfile = await MemberProfileService.sharedInstance.getByEmail(contact.email);
+        const statuses: ContactStatus = {
+          isMember: (memberProfile ? true : false),
+          isFriend: (memberProfile && friendIds.includes(memberProfile.cactusMemberId) ? true : false),
+          isRequested: (memberProfile && requestedFriendIds.includes(memberProfile.cactusMemberId) ? true : false)
+        };
 
-    for (const contact of emailContacts) {
-      const memberProfile = await MemberProfileService.sharedInstance.getByEmail(contact.email);
+        const preparedContact: ImportedContact = {
+          email_contact: contact,
+          statuses: statuses,
+          memberId: memberProfile?.cactusMemberId,
+          avatarUrl: memberProfile?.avatarUrl
+        };
 
-      const statuses: ContactStatus = {
-        isMember: (memberProfile ? true : false),
-        isFriend: (memberProfile && friendIds.includes(memberProfile.cactusMemberId) ? true : false),
-        isRequested: (memberProfile && requestedFriendIds.includes(memberProfile.cactusMemberId) ? true : false)
-      }
-
-      const preparedContact: ImportedContact = {
-        email_contact: contact,
-        statuses: statuses,
-        memberId: memberProfile?.cactusMemberId,
-        avatarUrl: memberProfile?.avatarUrl
-      }
-
-      preparedContacts.push(preparedContact);
-    };
+        resolve(preparedContact);
+      })
+    })); 
 
     return this.sortContacts(preparedContacts);
   }
