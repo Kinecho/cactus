90081639b0978e00d94ac0fe4d8e07531dad9c88
Scott Rocher <scottrocher@gmail.com>
2020-01-07 12:39:57 -0800

    Remove "done" status from entry cards

--- web/src/scripts/components/JournalEntryPromptContentCard.vue
+++ web/src/scripts/components/JournalEntryPromptContentCard.vue
@@ -1,7 +1,6 @@
 <template>
     <skeleton-card v-if="!allLoaded" :sentPrompt="sentPrompt"/>
     <div v-else class="journalEntry" v-bind:class="{new: !completed, isDone: completed, hasNote: hasNote}">
-        <div class="doneStatus" v-show="entry.responsesLoaded && completed">{{promptCopy.DONE}}</div>
         <p class="date">{{promptDate}}</p>
         <div class="menuParent">
             <dropdown-menu :items="linkItems"/>
