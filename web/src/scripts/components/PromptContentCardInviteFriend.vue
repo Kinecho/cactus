b2b6a533262738aedb1bd48eabcbd04f3b8da5b6
Katie Blackman <katie@cactus.app>
2020-01-14 15:41:00 -0700

    pull in card component, add icon to button, style up the activity card

--- web/src/scripts/components/PromptContentCardInviteFriend.vue
+++ web/src/scripts/components/PromptContentCardInviteFriend.vue
@@ -1,9 +1,20 @@
 <template>
     <div class="invite-friends">
         <div class="cta" v-if="!showInviteForm">
+            <SocialActivityCard
+                avatarUrl="/assets/images/avatars/blobatar1.png"
+                date="7 minutes ago"
+                name="Aaron Nichols"
+                promptContentPath="https://cactus.app/prompts/zTVQbvum95ENWV2Do3xE"
+                promptQuestion="Who energizes you?"/>
             <h2>Mindful Friends</h2>
-            <p>Your mindfulness journey is more effective when you share it with others.</p>
-            <button @click="showInviteForm = true">Invite a Friend</button>
+            <p class="subtext">Your mindfulness journey is more effective when you share it with others.</p>
+            <button class="button" @click="showInviteForm = true">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path fill="#fff" d="M12 14a5 5 0 015 5v2a1 1 0 01-2 0v-2a3 3 0 00-3-3H5a3 3 0 00-3 3v2a1 1 0 01-2 0v-2a5 5 0 015-5zm8-7a1 1 0 011 1l-.001 1.999L23 10a1 1 0 01.993.883L24 11a1 1 0 01-1 1l-2.001-.001L21 14a1 1 0 01-.883.993L20 15a1 1 0 01-1-1l-.001-2.001L17 12a1 1 0 01-.993-.883L16 11a1 1 0 011-1l1.999-.001L19 8a1 1 0 01.883-.993zM8.5 2a5 5 0 110 10 5 5 0 010-10zm0 2a3 3 0 100 6 3 3 0 000-6z"/>
+                </svg>
+                Invite a Friend
+            </button>
         </div>
         <div class="inviting" v-if="showInviteForm">
             <h2>Invite a Friend</h2>
@@ -26,10 +37,14 @@
     import {getDeviceDimensions, MOBILE_BREAKPOINT_PX} from "@web/DeviceUtil"
     import {isValidEmail} from "@shared/util/StringUtil"
     import {Image} from '@shared/models/PromptContent'
+    import SocialActivityCard from "@components/SocialActivityCard.vue"
 
     const copy = CopyService.getSharedInstance().copy;
 
     export default Vue.extend({
+        components: {
+            SocialActivityCard
+        },
         data(): {
             resizeListener: any | undefined,
             deviceWidth: number,
@@ -82,6 +97,37 @@
     @import "forms";
     @import "common";
     @import "transitions";
+
+    .activityCard {
+        background-color: white;
+        border: 0;
+        border-radius: 12px;
+        box-shadow: 0 5px 11px -3px rgba(0, 0, 0, 0.08);
+        margin: 0 0 3.2rem;
+
+        @include r(374) {
+            margin: 0 2.4rem 3.2rem;
+        }
+    }
+
+    .subtext {
+        margin: 0 auto 2.4rem;
+        max-width: 40rem;
+    }
+
+    .button {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        margin: 0 auto;
+        min-width: 22rem;
+
+        svg {
+            height: 2rem;
+            margin-right: .8rem;
+            width: 2rem;
+        }
+    }
 
     // .inviting {
     //     text-align: left;
