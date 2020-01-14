a4651ea0406b6227deb6ddfb5fc5c8150f2773c3
Scott Rocher <scottrocher@gmail.com>
2020-01-13 16:07:28 -0800

    Setup invite state elements / error interaction

--- web/src/scripts/components/PromptContentCardInviteFriend.vue
+++ web/src/scripts/components/PromptContentCardInviteFriend.vue
@@ -1,8 +1,21 @@
 <template>
     <div class="invite-friends">
-        <h2>Mindful Friends</h2>
-        <p>Your mindfulness journey is more effective when you share it with others.</p>
-        <button>Invite a friend</button>
+        <div class="cta" v-if="!showInviteForm">
+            <h2>Mindful Friends</h2>
+            <p>Your mindfulness journey is more effective when you share it with others.</p>
+            <button @click="showInviteForm = true">Invite a friend</button>
+        </div>
+        <div class="inviting" v-if="showInviteForm">
+            <h2>Invite a friend</h2>
+            <div>
+                <input v-model="emailAddress" @blur="validateEmail()" type="text" placeholder="Email address" /><br>
+                <span class="error" v-if="validEmail === false">Email address is invalid.</span>
+            </div>
+            <div>
+                <textarea placeholder="Optional message from you" rows="5"/>
+            </div>
+            <button @click="sendInvite()">Invite</button>
+        </div>
     </div>
 </template>
 
@@ -11,6 +24,7 @@
     import {formatDate} from '@shared/util/DateUtil'
     import CopyService from "@shared/copy/CopyService"
     import {getDeviceDimensions, MOBILE_BREAKPOINT_PX} from "@web/DeviceUtil"
+    import {isValidEmail} from "@shared/util/StringUtil"
     import {Image} from '@shared/models/PromptContent'
 
     const copy = CopyService.getSharedInstance().copy;
@@ -19,10 +33,16 @@
         data(): {
             resizeListener: any | undefined,
             deviceWidth: number,
+            showInviteForm: boolean,
+            emailAddress: string,
+            validEmail: boolean | undefined,
         } {
             return {
                 resizeListener: undefined,
                 deviceWidth: 0,
+                showInviteForm: false,
+                emailAddress: '',
+                validEmail: undefined
             }
         },
         destroyed() {
@@ -35,6 +55,22 @@
             this.resizeListener = window.addEventListener("resize", () => {
                 this.deviceWidth = getDeviceDimensions().width;
             })
+        },
+        methods: {
+            sendInvite() {
+                this.validateEmail();
+                
+                if (this.validEmail) {
+                    console.log('would send invite');
+                }
+            },
+            validateEmail() {
+                if (isValidEmail(this.emailAddress)) {
+                    this.validEmail = true;
+                } else { 
+                    this.validEmail = false;
+                }
+            }
         }
     })
 </script>
@@ -47,5 +83,30 @@
     @import "common";
     @import "transitions";
 
+    .inviting {
+        text-align: left;
+
+        input {
+            width: 80%;
+            padding: 1rem;
+        }
+        .add {
+            font-size: 80%;
+            margin: 0 0 2rem;
+            display: block;
+        }
+        .error {
+            color: red;
+            display: block;
+            margin: 0 0 1rem;
+        }
+        textarea {
+            width: 80%;
+            padding: 1rem;
+        }
+        button {
+            margin: 3rem 0 0;
+        }
+    }
 
 </style>
