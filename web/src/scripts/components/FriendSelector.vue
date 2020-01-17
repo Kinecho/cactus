266f41ef8a2d163d3f5ed016c676b89800a7f1f5
Scott Rocher <scottrocher@gmail.com>
2020-01-17 13:55:40 -0800

    Manage select / deselect from friends

--- web/src/scripts/components/FriendSelector.vue
+++ web/src/scripts/components/FriendSelector.vue
@@ -2,7 +2,7 @@
     <div>
       <div v-for="connection of connections">
         <SocialFriend :member="member" :connection="connection" />
-        <input type="checkbox">
+        <input @click="toggleFriend(connection.friendMemberId, $event)" type="checkbox">
       </div>
     </div>
 </template>
@@ -14,6 +14,9 @@
     import SocialFriend from '@components/SocialFriend.vue'
     import {getIntegerFromStringBetween} from '@shared/util/StringUtil';
     import SocialConnectionService from '@web/services/SocialConnectionService'
+    import Logger from "@shared/Logger";
+
+    const logger = new Logger("PromptContentCardReciprocalSharing.vue");
 
     export default Vue.extend({
         components: {
@@ -28,13 +31,36 @@
           } 
         },
         data(): {
-            connections: SocialConnection[],
-            selectedConnections: string[]
+            connections: SocialConnection[] | undefined,
+            selectedFriends: string[]
         } {
             return {
-              connections: [],
-              selectedConnections: []
+              connections: undefined,
+              selectedFriends: []
             }
+        },
+        methods: {
+          toggleFriend(id: string, e: MouseEvent) {
+            if (e.target) {
+              const checkbox = e.target as HTMLInputElement;
+              if (checkbox?.checked) {
+                this.selectFriend(id);
+              } else {
+                this.deselectFriend(id);
+              }
+              logger.log(this.selectedFriends);
+            }
+          },
+          selectFriend(id: string) {
+            this.selectedFriends.push(id);
+          },
+          deselectFriend(id: string) {
+            this.selectedFriends = this.selectedFriends.filter(
+              function(selectedFriendId: string){
+                return selectedFriendId != id;
+              }
+            );
+          }
         }
     })
 </script>
