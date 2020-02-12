93920c949cb2720db2606560f4802a9cf34ab88b
Katie Blackman <katie@cactus.app>
2020-02-12 11:29:44 -0700

    remove extra containing div, refactor to not use radio inputs, remove css, remove grid

--- web/src/scripts/components/PremiumPricing.vue
+++ web/src/scripts/components/PremiumPricing.vue
@@ -1,18 +1,18 @@
 <template>
-    <section class="premium">
-        <div class="centered">
-            <div class="tabset">
-                <input type="radio" name="tabset" id="tab1" aria-controls="free" :checked="!premiumDefault">
-                <label class="tab-label free-tab" for="tab1">Basic</label>
-                <section id="free" class="tab-panel free-panel">
+    <div class="centered">
+        <div id="tabs" class="tabset">
+            <div class="tabs">
+                <a class="tab-label" v-on:click="activetab='1'" v-bind:class="[ activetab === '1' ? 'active' : '' ]" aria-controls="basic">Basic</a>
+                <a class="tab-label" v-on:click="activetab='2'" v-bind:class="[ activetab === '2' ? 'active' : '' ]" aria-controls="plus">Plus</a>
+            </div>
+
+            <div class="tab-panel">
+                <div v-if="activetab ==='1'" class="tabcontent">
                     <p>Receive new prompts occasionally (~ once a&nbsp;week)</p>
                     <p class="price">Free forever</p>
                     <button class="secondary" disabled>Current Plan</button>
-                </section>
-
-                <input type="radio" name="tabset" id="tab2" aria-controls="premium" :checked="premiumDefault">
-                <label class="tab-label premium-tab" for="tab2">Plus</label>
-                <section id="premium" class="tab-panel premium-panel">
+                </div>
+                <div v-if="activetab ==='2'" class="tabcontent">
                     <p>Every day, there’s a new prompt waiting for&nbsp;you</p>
                     <div class="flexContainer">
                         <template v-for="plan in plans">
@@ -27,10 +27,13 @@
                         ${{selectedPlan.price_dollars}} / {{selectedPlan.per}}
                     </button>
                     <p class="heart">Supports Cactus development</p>
-                </section>
+                </div>
             </div>
         </div>
-    </section>
+
+        <!-- <input type="radio" name="tabset" id="tab1" aria-controls="free" :checked="!premiumDefault">
+        <input type="radio" name="tabset" id="tab2" aria-controls="premium" :checked="premiumDefault"> -->
+    </div>
 </template>
 
 <script lang="ts">
@@ -85,6 +88,7 @@
             memberEmail: string | undefined,
             memberUnsubscriber: ListenerUnsubscriber | undefined,
             premiumDefault: boolean,
+            activetab: number[],
         } {
             return {
                 selectedPlan: this.plans[0],
@@ -93,6 +97,7 @@
                 memberEmail: undefined,
                 memberUnsubscriber: undefined,
                 premiumDefault: false,
+                activetab: [1, 2]
             }
         },
         beforeMount() {
@@ -161,34 +166,22 @@
     @import "variables";
     @import "transitions";
 
-    .premium {
-        background: $beige;
-        padding: 4.8rem 0;
+    .centered {
+        overflow: hidden;
+        position: relative;
 
-        @include r(768) {
-            padding: 9rem 2.4rem;
-        }
-
-        .centered {
-            overflow: hidden;
-            position: relative;
-
-            @include r(960) {
-                display: flex;
-                flex-direction: row-reverse;
-                justify-content: center;
-                text-align: left;
-            }
+        @include r(960) {
+            display: flex;
+            flex-direction: row-reverse;
+            justify-content: center;
+            text-align: left;
         }
     }
 
     .tabset {
         background-color: $darkestGreen;
         border-radius: 1.2rem;
-        display: grid;
-        grid-template-areas: "tab1 tab2" "tabpanel tabpanel";
-        grid-template-columns: 50%;
-        position: relative;
+        color: $white;
         text-align: left;
 
         @include r(374) {
@@ -201,7 +194,6 @@
         }
         @include r(768) {
             background-color: transparent;
-            grid-template-areas: "tabpanel1 tabpanel2";
             min-width: 67rem;
         }
         @include r(960) {
@@ -209,67 +201,38 @@
         }
     }
 
-    /* hide the radios, show the panels */
-    .tabset input[type="radio"] {
-        position: absolute;
-        left: -200vw;
+    .tabs {
+        display: flex;
 
-        @include r(768) {
-            display: none;
-        }
-    }
+        .tab-label {
+            background-color: darken($darkestGreen, 5%);
+            color: $white;
+            flex-grow: 1;
+            font-size: 2rem;
+            font-weight: bold;
+            padding: 1.6rem;
+            text-align: center;
 
-    .tabset .tab-panel,
-    .tab-header {
-        display: none;
+            &:first-child {
+                border-radius: 1.2rem 0 0 0;
+            }
+            &:last-child {
+                border-radius: 0 1.2rem 0 0;
+            }
 
-        @include r(768) {
-            display: block;
-        }
-    }
+            &:hover {
+                background-color: darken($darkestGreen, 3%);
+                cursor: pointer;
+            }
 
-    .tabset > input:nth-of-type(1):checked ~ .tab-panel:nth-of-type(1),
-    .tabset > input:nth-of-type(2):checked ~ .tab-panel:nth-of-type(2) {
-        display: block;
-    }
-
-    .tab-label {
-        background-color: darken($darkestGreen, 5%);
-        cursor: pointer;
-        font-size: 2rem;
-        font-weight: bold;
-        padding: 1.6rem;
-        text-align: center;
-
-        &:nth-of-type(1) {
-            border-radius: 1.2rem 0 0 0;
+            &.active {
+                background-color: $darkestGreen;
+            }
         }
 
-        &:nth-of-type(2) {
-            border-radius: 0 1.2rem 0 0;
-        }
-
-        @include r(768) {
-            display: none;
-        }
-    }
-
-    .tabset > .tab-label:hover {
-        background-color: darken($darkestGreen, 3%);
-    }
-
-    .tabset > input:focus + .tab-label,
-    .tabset > input:checked + .tab-label {
-        background-color: $darkestGreen;
-    }
-
-    .tab-header {
-        font-size: 2.4rem;
-        margin-bottom: 2.4rem;
     }
 
     .tab-panel {
-        grid-area: tabpanel;
         padding: 2.4rem 2.4rem 3.2rem;
 
         @include r(768) {
@@ -280,7 +243,6 @@
                 background-color: $white;
                 border-radius: 1.8rem 0 0 0;
                 color: $darkestGreen;
-                grid-area: tabpanel1;
 
                 ul {
                     margin-bottom: 7.2rem;
@@ -289,7 +251,6 @@
             &.premium-panel {
                 background-color: $darkestGreen;
                 border-radius: 1.8rem 1.8rem 0 0;
-                grid-area: tabpanel2;
             }
         }
 
