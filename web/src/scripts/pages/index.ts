729db4e3d538611cdc1f85d47dfcc74bc3b9d72f
Scott Rocher <scottrocher@gmail.com>
2020-03-11 11:37:14 -0700

    Hide "try it free" header if on CactusAndroid app

--- web/src/scripts/pages/index.ts
+++ web/src/scripts/pages/index.ts
@@ -8,7 +8,7 @@ import PremiumPricing from "@components/PremiumPricing.vue";
 import AppStoreIcon from "@components/AppStoreIcon.vue";
 import PlayStoreIcon from "@components/PlayStoreIcon.vue";
 import StandardFooter from "@components/StandardFooter.vue";
-import {isIosDevice, isAndroidDevice} from '@web/DeviceUtil';
+import {isIosDevice, isAndroidDevice, isAndroidApp} from '@web/DeviceUtil';
 import {commonInit} from "@web/common";
 import Logger from "@shared/Logger";
 
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
 const $tryItHeader = document.getElementById("tryItHeader");
 const isMobileDevice = (isAndroidDevice() || isIosDevice());
 if ($tryItHeader) {
-    $tryItHeader.classList.add(isMobileDevice ? 'show' : 'hide');
+    $tryItHeader.classList.add(isMobileDevice && !isAndroidApp ? 'show' : 'hide');
 } 
 
 // new Vue({
