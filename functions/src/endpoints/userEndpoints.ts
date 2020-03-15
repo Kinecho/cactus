9679b9143886a5e9b4de9360ad1e8db3d8a055d4
Scott Rocher <scottrocher@gmail.com>
2020-03-15 15:41:17 -0700

    Update core values auth messaging and add Slack activity logging

--- functions/src/endpoints/userEndpoints.ts
+++ functions/src/endpoints/userEndpoints.ts
@@ -10,6 +10,7 @@ import {QueryParam} from "@shared/util/queryParams";
 import {PageRoute} from "@shared/PageRoutes";
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import AdminSlackService from "@admin/services/AdminSlackService";
 import Logger from "@shared/Logger";
 
 const logger = new Logger("userEndpoints");
@@ -72,7 +73,16 @@ app.get("/feature-auth/core-values", async (req: functions.https.Request | any, 
     if (!memberId) {
         // show an error message and instructions to upgrade
         logger.log("No memberId provided");
-        resp.status(401).contentType("text/html").send('<html><body style="font-family: sans-serif; text-align: center; padding: 3rem 1rem; line-height: 150%;"><h3>To access Core Values, use the latest version of Cactus.</h3><br><p><strong>iOS Users:</strong> <a style="color:#29A389" href="https://apps.apple.com/us/app/cactus-mindfulness-journal/id1474513514">Follow this link</a> to upgrade your app.<br><br>Still having problems? Email <a style="color:#29A389" href="mailto:help@cactus.app">help@cactus.app</a></p></body></html>');
+        resp.status(401).contentType("text/html").send('<html><body style="font-family: sans-serif; text-align: center; padding: 3rem 1rem; line-height: 150%;"><h3>To access Core Values, use must be logged-in to the latest version of Cactus.</h3><br><p><strong>iOS Users:</strong> <a style="color:#29A389" href="https://apps.apple.com/us/app/cactus-mindfulness-journal/id1474513514">Follow this link</a> to upgrade your app.<br><br>Still having problems? Email <a style="color:#29A389" href="mailto:help@cactus.app">help@cactus.app</a></p></body></html>');
+        
+        try {
+            await AdminSlackService.getSharedInstance().sendActivityMessage({
+                text: `Someone without a memberId tried to auth to Core Values.`
+            });
+        } catch(e) {
+            logger.error(e);
+        }
+
         return;
     }
 
@@ -87,10 +97,28 @@ app.get("/feature-auth/core-values", async (req: functions.https.Request | any, 
     if (member?.tier == SubscriptionTier.PLUS && member?.email) {
         logger.log('Member is PLUS. Redirecting to Core Values survey...');
         resp.redirect('https://www.surveymonkey.com/r/core-values-v1?email=' + member.email);
+
+        try {
+            await AdminSlackService.getSharedInstance().sendActivityMessage({
+                text: `PLUS member ${member.email} authorized to Core Values`
+            });
+        } catch(e) {
+            logger.error(e);
+        }
+
         return;
     } else {
         const pricingUrl = `${getHostname()}${PageRoute.PAYMENT_PLANS}?${QueryParam.CORE_VALUES}=true`;
         resp.redirect(pricingUrl);
+
+        try {
+            await AdminSlackService.getSharedInstance().sendActivityMessage({
+                text: `BASIC member ${member.email} authorized to Core Values. Redirected to Upgrade Page.`
+            });
+        } catch(e) {
+            logger.error(e);
+        }
+
         return;
     }
 
