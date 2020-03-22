82d99f146ae8e4bbfbd5e2f18decf6f080220ab2
Katie Blackman <katie@cactus.app>
2020-03-22 17:02:46 -0600

    merge styles with scott’s modal work, ready for more styles

--- web/src/scripts/components/PricingModal.vue
+++ web/src/scripts/components/PricingModal.vue
@@ -3,44 +3,32 @@
             v-on:close="$emit('close')"
             :showCloseButton="true"
     >
-    <div slot="body" class="modalContainer">
-      <section class="hero">
-        <div class="centered">
-            <h1>Keep going with Cactus&nbspPlus</h1>
-            <p class="subtext">With Cactus Plus, you'll get immediate access to today's prompt, and these popular features:</p>
+    <div slot="body" class="miniModal">
+        <h2>Keep going with Cactus&nbsp;Plus</h2>
+        <p class="subtext">With Cactus Plus, you'll get immediate access to today's prompt, and these popular features:</p>
+        <div class="benefit">
+            <span class="benefitIcon"><img src="/assets/images/calendar.svg" alt=""/></span>
+            <div>
+                <h3>Make it daily</h3>
+                <p class="text">Improve your focus and positivity at work and home with a fresh prompt, every&nbsp;day.</p>
+            </div>
         </div>
-      </section>
-      <div class="middleSections">
-          <section class="benefits">
-              <div class="centered">
-                <div>
-                  <div class="flexContainer">
-                        <div class="benefit">
-                            <span class="benefitIcon"><img src="/assets/images/calendar.svg" alt=""/></span>
-                            <h3>Make it daily</h3>
-                            <p class="text">Improve your focus and positivity at work and home with a fresh prompt, every&nbsp;day.</p>
-                        </div>
-                        <div class="benefit">
-                            <span class="benefitIcon"><img src="assets/images/journal.svg" alt=""/></span>
-                            <h3>Personal Insights</h3>
-                            <p class="text">Visualizations reveal the people, places, and things that contribute to your&nbsp;satisfaction.</p>
-                        </div>
-                        <div class="benefit">
-                            <span class="benefitIcon"><img src="/assets/images/lock.svg" alt=""/></span>
-                            <h3>Core Values Assessment</h3>
-                            <p class="text">Make better decisions by prioritizing what's important to you.</p>
-                        </div>
-                    </div>
-                  </div>
-              </div>
-          </section>
-      </div>
-      <section class="upgrade">
-          <div class="centered" id="upgrade">
-              <h2 class="sectionHeader">Choose your plan:</h2>
-              <PremiumPricing :startTrial="true" />
-          </div>
-      </section>
+        <div class="benefit">
+            <span class="benefitIcon"><img src="/assets/images/journal.svg" alt=""/></span>
+            <div>
+                <h3>Personal Insights</h3>
+                <p class="text">Visualizations reveal the people, places, and things that contribute to your&nbsp;satisfaction.</p>
+            </div>
+        </div>
+        <div class="benefit">
+            <span class="benefitIcon"><img src="/assets/images/lock.svg" alt=""/></span>
+            <div>
+                <h3>Core Values Assessment</h3>
+                <p class="text">Make better decisions by prioritizing what's important to you.</p>
+            </div>
+        </div>
+        <PremiumPricing :startTrial="true" />
+        <a class="button secondary" href="#upgrade">Learn More</a>
     </div>
   </modal>
 </template>
@@ -70,33 +58,49 @@
   @import "~styles/common";
   @import "~styles/mixins";
   @import "~styles/transitions";
-  /*@import "~styles/pages/pricing";*/
-  
-  .modalContainer {
-      background-color: $white;
-      border-radius: 1.2rem;
-      display: flex;
-      min-height: 34rem;
-      overflow: hidden;
-      width: 48rem;
-  }
+  @import "~styles/pages/pricing";
 
-  .top-message {
-      border-radius: 0;
-      display: block;
-      padding: 3.2rem 2.4rem;
-      margin-bottom: 0;
+  .miniModal {
+      @include shadowbox;
+      background-color: $beige;
+      min-height: 100vh;
+      padding: .8rem;
+      text-align: center;
 
-      .centered {
+      @include r(374) {
+          padding: 2.4rem;
+      }
+      @include r(600) {
+          min-height: 0;
+          padding: 3.2rem;
+      }
+
+      .subtext {
+          margin: 0 auto 4rem;
           max-width: 64rem;
       }
 
-      p {
-          opacity: .9;
+      .benefit {
+          display: flex;
+          margin: 0 0 1.6rem;
+          max-width: none;
+          text-align: left;
+
+          @include r(600) {
+              align-items: center;
+          }
+
+          &:last-of-type {
+              margin-bottom: 4rem;
+          }
       }
 
-      a {
-          @include fancyLinkLight;
+      h3 {
+          margin-bottom: 0;
+      }
+
+      .benefitIcon {
+          margin: 0;
       }
   }
 </style>
