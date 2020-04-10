60d9a3f1b8ce28a23d57e2053bd1de77a8526353
Scott Rocher <scottrocher@gmail.com>
2020-04-10 11:10:27 -0700

    Add PLUS badge to Download Data setting

--- web/src/scripts/components/AccountSettings.vue
+++ web/src/scripts/components/AccountSettings.vue
@@ -99,7 +99,7 @@
                     </div>
 
                     <div class="settings-group" v-if="showExportData">
-                        <h2>Download Data</h2>
+                        <h2>Download Data<span class="badge-label">Plus</span></h2>
                         <p class="subtext">You can export and download your data from Cactus, including all your journal prompts and written reflections. Tap the <strong>Download</strong> button below to create an archive to keep for your records or use the data in another service.
                         <div class="item">
                             <DataExport :member="member"/>
@@ -522,6 +522,12 @@
         color: $royal;
         font-size: 2.4rem;
         margin-bottom: 2.4rem;
+
+        .badge-label {
+            @include trialBadge;
+            color: $white;
+            vertical-align: text-bottom;
+        }
     }
 
     .subtext {
