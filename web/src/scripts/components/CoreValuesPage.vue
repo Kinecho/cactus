35ba55ea24d13797364968d677b78c778d87e89f
Katie Blackman <katie@cactus.app>
2020-04-10 19:31:23 -0600

    set up states incorrectly

--- web/src/scripts/components/CoreValuesPage.vue
+++ web/src/scripts/components/CoreValuesPage.vue
@@ -2,11 +2,24 @@
     <div class="coreValuesPage">
         <NavBar :isSticky="false"/>
         <div class="centered">
-            <h1>Core Values</h1>
-            <p>Core values are the general expression of what is most important for you, and they help you understand past decisions and make better decisions in the future.</p>
-            <p>Knowing your core values is just the beginning. Cactus will help you prioritize a deeper exploration of how your values have been at the heart of past decisions and how they will unlock a happier future. Your core values results will guide your Cactus reflections.</p>
-            <p>Insert language about how long this will take or how many questions to set expectations...</p>
-            <button>Take the Assessment</button>
+            <template v-if="plusUser && !hasValues">
+                <h1>Core Values</h1>
+                <p>Core values are the general expression of what is most important for you, and they help you understand past decisions and make better decisions in the future.</p>
+                <p>Knowing your core values is just the beginning. Cactus will help you prioritize a deeper exploration of how your values have been at the heart of past decisions and how they will unlock a happier future. Your core values results will guide your Cactus reflections.</p>
+                <p>Insert language about how long this will take or how many questions to set expectations...</p>
+                <button>Take the Assessment</button>
+            </template>
+            <template v-if="!plusUser">
+                <h1>Core Values ain't free</h1>
+                <p>Core values are the general expression of what is most important for you, and they help you understand past decisions and make better decisions in the future.</p>
+                <p>Knowing your core values is just the beginning. Cactus will help you prioritize a deeper exploration of how your values have been at the heart of past decisions and how they will unlock a happier future. Your core values results will guide your Cactus reflections.</p>
+                <p>Insert language about how long this will take or how many questions to set expectations...</p>
+                <button>Upgrade</button>
+            </template>
+            <template v-if="plusUser && hasValues">
+                <h1>Core Values</h1>
+                <p>Here are your core values. Through the Cactus prompts, you will come to better understand the origin, purpose, and meaning of your core values. This will help you understand past life decisions and, by prioritizing your values, make better decisions in the future.</p>
+            </template>
         </div>
         <Footer/>
     </div>
@@ -22,6 +35,7 @@
 
     declare interface CoreValuesData {
         cactusMember?: CactusMember,
+        hasValues: boolean,
     }
 
     export default Vue.extend({
@@ -38,6 +52,7 @@
         data(): CoreValuesData {
             return {
                 cactusMember: undefined,
+                hasValues: false,
             };
         },
         computed: {
