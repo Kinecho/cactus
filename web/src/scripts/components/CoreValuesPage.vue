8c093474f75f97167e209a2d54f3bc2a1dcf0263
Katie Blackman <katie@cactus.app>
2020-04-13 12:30:01 -0600

    added upgrade button, styled core values card, update fancy link colors

--- web/src/scripts/components/CoreValuesPage.vue
+++ web/src/scripts/components/CoreValuesPage.vue
@@ -2,32 +2,41 @@
     <div class="coreValuesPage">
         <NavBar :isSticky="false"/>
         <div class="centered">
+            <h1>Core Values</h1>
+            <!-- TODO: make booleans plusUser and hasValues work -->
             <template v-if="plusUser && !hasValues">
-                <h1>Core Values</h1>
                 <p>Core values are the general expression of what is most important for you, and they help you understand past decisions and make better decisions in the future.</p>
                 <p>Knowing your core values is just the beginning. Cactus will help you prioritize a deeper exploration of how your values have been at the heart of past decisions and how they will unlock a happier future. Your core values results will guide your Cactus reflections.</p>
                 <p>Insert language about how long this will take or how many questions to set expectations...</p>
-                <button>Take the Assessment</button>
+                <!-- TODO: hook up button -->
+                <button class="primaryBtn">Take the Assessment</button>
             </template>
             <template v-if="!plusUser">
-                <h1>Core Values ain't free</h1>
-                <p>Core values are the general expression of what is most important for you, and they help you understand past decisions and make better decisions in the future.</p>
+                <p>Different language? Core values are the general expression of what is most important for you, and they help you understand past decisions and make better decisions in the future.</p>
                 <p>Knowing your core values is just the beginning. Cactus will help you prioritize a deeper exploration of how your values have been at the heart of past decisions and how they will unlock a happier future. Your core values results will guide your Cactus reflections.</p>
                 <p>Insert language about how long this will take or how many questions to set expectations...</p>
-                <button>Upgrade</button>
+                <button class="primaryBtn" @click="goToPricing">Upgrade</button>
             </template>
             <template v-if="plusUser && hasValues">
-                <h1>Core Values</h1>
                 <p>Here are your core values. Through the Cactus prompts, you will come to better understand the origin, purpose, and meaning of your core values. This will help you understand past life decisions and, by prioritizing your values, make better decisions in the future.</p>
                 <figure class="coreValuesCard">
-                    <h3>Mike's Core Values</h3>
+                    <!-- TODO: hook up displayName -->
+                    <h3><span class="cvName" v-if="displayName">{{displayName}}'s</span>Core Values</h3>
                     <div class="flexContainer">
-                        <p>rando flamelink image</p>
+                        <!-- TODO: insert random blob here -->
+                        <img class="cvBlob" src="https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2Fsized%2F375_9999_99%2F200411.png?alt=media&token=6f2c2d46-d282-4c1a-87de-9259136c79a0" alt="core value blob graphic" />
+                        <!-- TODO: make this list dynamic -->
                         <ul class="valuesList">
+                            <li>Developer</li>
+                            <li>Developer</li>
+                            <li>Developer</li>
+                            <li>Developer</li>
                             <li>Developer</li>
                         </ul>
                     </div>
                 </figure>
+                <button class="small">Share My Values</button>
+                <p class="extraPadding">Not sure these are right or feel like they’ve changed? Feel free to <a class="fancyLink" href="">retake the assessment</a>.</p>
             </template>
         </div>
         <Footer/>
@@ -41,10 +50,12 @@
     import {SubscriptionTier} from "@shared/models/SubscriptionProductGroup";
     import CactusMember from '@shared/models/CactusMember'
     import CactusMemberService from '@web/services/CactusMemberService'
+    import {PageRoute} from '@shared/PageRoutes'
 
     declare interface CoreValuesData {
         cactusMember?: CactusMember,
         hasValues: boolean,
+        displayName: string,
     }
 
     export default Vue.extend({
@@ -62,7 +73,13 @@
             return {
                 cactusMember: undefined,
                 hasValues: false,
+                displayName: '',
             };
+        },
+        methods: {
+            goToPricing() {
+                window.location.href = PageRoute.PRICING;
+            },
         },
         computed: {
             plusUser(): boolean {
@@ -104,44 +121,88 @@
             margin-bottom: 1.6rem;
             max-width: 64rem;
         }
+    }
 
-        button {
-            display: block;
-            margin-top: 2.4rem;
-            width: 100%;
+    .primaryBtn {
+        display: block;
+        margin-top: 2.4rem;
+        width: 100%;
 
-            @include r(600) {
-                width: auto;
-            }
+        @include r(600) {
+            width: auto;
         }
     }
 
     .coreValuesCard {
-        background-color: $dolphin;
-        border-radius: 1.2rem;
+        @include shadowbox;
+        background-color: darken($royal, 6%);
         color: white;
-        display: inline-block;
-        margin: 0 0 3.2rem;
+        margin: 1.6rem auto 2.4rem;
+        max-width: fit-content;
         padding: 2.4rem 0;
         text-align: center;
         width: auto;
+
+        @include r(768) {
+            margin: 3.2rem 0;
+        }
 
         .flexContainer {
             display: flex;
         }
 
-        ul, li {
+        .cvName {
+            padding-right: .4rem;
+        }
+
+        ul {
+            align-self: center;
             list-style: none;
-            margin: 0;
-            padding: 0;
+            margin: 0 0 0 1.6rem;
+            padding: 0 1.6rem 0 0;
+            text-align: center;
+            width: 100%;
         }
 
         li {
             font-size: 1.4rem;
             font-weight: bold;
             letter-spacing: 1px;
+            list-style: none;
+            margin: 0 0 .8rem;
+            padding: 0;
             text-transform: uppercase;
         }
+
+        h3 {
+            margin-bottom: 1.6rem;
+        }
+    }
+
+    .cvBlob {
+        align-self: flex-start;
+        height: auto;
+        margin-bottom: -3.2rem;
+        max-width: 20rem;
+        transform: translate(-.8rem);
+        width: 50%;
+    }
+
+    button.small {
+        display: flex;
+        margin: 0 auto;
+
+        @include r(768) {
+            margin: 0;
+        }
+    }
+
+    .fancyLink {
+        @include fancyLink;
+    }
+
+    .extraPadding {
+        margin-top: 6.4rem;
     }
 
 </style>
