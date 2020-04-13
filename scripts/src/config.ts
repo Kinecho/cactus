e7cd21754e28af6f1b0b3b5891b91505723ca389
Neil Poulin <neil.j.poulin@gmail.com>
2020-04-13 15:32:11 -0600

    get firebase tools upgrade working

--- scripts/src/config.ts
+++ scripts/src/config.ts
@@ -1,12 +1,12 @@
-import {buildConfig} from "@admin/config/configService";
-
-const firebaseTools = require("firebase-tools");
+import { buildConfig } from "@admin/config/configService";
 import * as admin from "firebase-admin";
-
-const prompts = require("prompts");
 import chalk from "chalk";
 import helpers from "@scripts/helpers";
-import {CactusConfig} from "@shared/CactusConfig";
+import { CactusConfig } from "@shared/CactusConfig";
+
+const firebaseTools = require("firebase-tools");
+
+const prompts = require("prompts");
 
 export enum Project {
     STAGE = "stage",
@@ -24,12 +24,12 @@ export const DefaultOptions: ConfigOptions = {
 export async function getAdmin(project: Project, opts: ConfigOptions = DefaultOptions): Promise<admin.app.App> {
     if (project === Project.PROD) {
         console.log(
-            chalk.blue.bgRed.bold(
-                "\n " +
-                "****************************************\n" +
-                " WARNING: This process will run in prod  \n" +
-                "**************************************** \n\n"
-            )
+        chalk.blue.bgRed.bold(
+        "\n " +
+        "****************************************\n" +
+        " WARNING: This process will run in prod  \n" +
+        "**************************************** \n\n"
+        )
         );
 
         const response = await prompts({
@@ -52,14 +52,20 @@ export async function getAdmin(project: Project, opts: ConfigOptions = DefaultOp
     return app;
 }
 
-export async function getAdminCredential(project: Project): Promise<admin.credential.Credential|undefined> {
+export async function getAdminCredential(project: Project): Promise<admin.credential.Credential | undefined> {
     // let funcConfig = functions.config();
-    const serviceAccount = await import(`${helpers.projectRoot}/applicationCredentials_${project}`);
+    const serviceAccount = await import(`${ helpers.projectRoot }/applicationCredentials_${ project }`);
     return admin.credential.cert(serviceAccount);
 }
 
 async function getAdminConfig(project: Project, opts: ConfigOptions = DefaultOptions) {
-    const config = await firebaseTools.setup.web({project: project});
+
+    const apps = await firebaseTools.apps.list("web", { project: project });
+    console.log("Firebase apps....", JSON.stringify(apps, null, 2));
+    const appId = apps[0].appId;
+    process.env.GCLOUD_PROJECT = apps[0].projectId;
+    console.log("app id", appId);
+    const config = await firebaseTools.apps.sdkconfig("web", appId, { project: project });
     if (opts && opts.useAdmin) {
         try {
             // let funcConfig = functions.config();
@@ -75,7 +81,7 @@ async function getAdminConfig(project: Project, opts: ConfigOptions = DefaultOpt
 }
 
 export async function getCactusConfig(project: Project): Promise<CactusConfig> {
-    const _config = await firebaseTools.functions.config.get(undefined, {project}) as CactusConfig;
+    const _config = await firebaseTools.functions.config.get(undefined, { project }) as CactusConfig;
     return buildConfig(_config);
 }
 
