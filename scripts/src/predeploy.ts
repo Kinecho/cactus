b6cd0f04d0a8ce7a026fa9c007fc345c556c2a42
Neil Poulin <neil.j.poulin@gmail.com>
2020-04-22 17:29:28 -0600

    comment out unused (for now) pre and post deploy code

--- scripts/src/predeploy.ts
+++ scripts/src/predeploy.ts
@@ -1,14 +1,12 @@
 import chalk from "chalk";
-import {getCactusConfig, Project} from "@scripts/config";
-import AdminSlackService, {ChatMessage} from "@admin/services/AdminSlackService";
+import { getCactusConfig, Project } from "@scripts/config";
 import helpers from "@scripts/helpers";
-import * as simplegit from 'simple-git/promise';
-import {CactusConfig} from "@shared/CactusConfig";
-import {getAppSiteConfig} from "@scripts/appleAppSiteAssociation";
-import {writeFileSync, copyFileSync} from "fs";
+import { CactusConfig } from "@shared/CactusConfig";
+import { getAppSiteConfig } from "@scripts/appleAppSiteAssociation";
+import { copyFileSync, writeFileSync } from "fs";
 import * as path from "path";
 
-const git = simplegit();
+// const git = simplegit();
 
 (async () => {
     const projectId = process.env.GCLOUD_PROJECT;
@@ -20,7 +18,7 @@ const git = simplegit();
         resource = process.argv[2];
     }
 
-    const envName = isProd ? 'Prod' : 'Stage';
+    // const envName = isProd ? 'Prod' : 'Stage';
     console.log(chalk.green(`${resource} Starting Deploy to ${isProd ? "Prod" : "Stage"}`));
 
     const config = await getCactusConfig(isProd ? Project.PROD : Project.STAGE);
@@ -29,39 +27,39 @@ const git = simplegit();
     createAppAppSiteAssociationFile(config);
     createAppleDeveloperDomainAssociationFile(config);
 
-    let resourceName = resource;
-    if (resource.toLowerCase().trim() === "hosting") {
-        resourceName = `<https://${config.web.domain}|Web>`
-    }
-    console.log(`got config: ${config.web.domain}`);
-
-
-    const username: string | undefined | null = await git.raw(['config', '--global', 'user.name']);
-    const email: string | undefined | null = await git.raw(['config', '--global', 'user.email']);
-
-    let byLine = "unknown";
-    if (username && email) {
-        byLine = `${username.trim()} \<${email.trim()}\>`;
-    } else if (username) {
-        byLine = username.trim();
-    } else if (email) {
-        byLine = email.trim();
-    }
-
-    console.log(`username ${username} | email ${email}`);
-
-    const hash = await git.revparse(["HEAD"]);
-
-    const message: ChatMessage = {
-        text: "",
-        attachments: [{
-            title: `Starting the deployment process for ${resourceName} - ${envName}`,
-            text: `<https://sentry.io/organizations/kinecho/releases/${hash}|Sentry Release>: \`${hash}\`\n_started by ${byLine}_`,
-            ts: `${(new Date()).getTime() / 1000}`
-        }],
-    };
-
-    AdminSlackService.initialize(config);
+    // let resourceName = resource;
+    // if (resource.toLowerCase().trim() === "hosting") {
+    //     resourceName = `<https://${config.web.domain}|Web>`
+    // }
+    // console.log(`got config: ${config.web.domain}`);
+    //
+    //
+    // const username: string | undefined | null = await git.raw(['config', '--global', 'user.name']);
+    // const email: string | undefined | null = await git.raw(['config', '--global', 'user.email']);
+    //
+    // let byLine = "unknown";
+    // if (username && email) {
+    //     byLine = `${username.trim()} \<${email.trim()}\>`;
+    // } else if (username) {
+    //     byLine = username.trim();
+    // } else if (email) {
+    //     byLine = email.trim();
+    // }
+    //
+    // console.log(`username ${username} | email ${email}`);
+    //
+    // const hash = await git.revparse(["HEAD"]);
+    //
+    // const message: ChatMessage = {
+    //     text: "",
+    //     attachments: [{
+    //         title: `Starting the deployment process for ${resourceName} - ${envName}`,
+    //         text: `<https://sentry.io/organizations/kinecho/releases/${hash}|Sentry Release>: \`${hash}\`\n_started by ${byLine}_`,
+    //         ts: `${(new Date()).getTime() / 1000}`
+    //     }],
+    // };
+    //
+    // AdminSlackService.initialize(config);
     // await AdminSlackService.getSharedInstance().sendCIMessage(message);
 
 })().then(() => console.log("Done")).catch(e => console.error("Failed to execute slack command", e));
