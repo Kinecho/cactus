c183e213caf86588d5a7a13a1faeb696ab79fb11
Neil Poulin <neil.j.poulin@gmail.com>
2020-04-27 16:45:24 -0600

    scroll to top when navigating to new route

--- web/src/scripts/router.ts
+++ web/src/scripts/router.ts
@@ -100,6 +100,10 @@ const router = new VueRouter({
     mode: "history",
     // base: process.env.BASE_URL,
     routes,
+    scrollBehavior (to, from, savedPosition) {
+        return { x: 0, y: 0 }
+    }
+
 });
 
 
