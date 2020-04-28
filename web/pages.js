04d5b1d0be839928231946b5cb60c8e3c299eab5
Neil Poulin <neil.j.poulin@gmail.com>
2020-04-27 23:25:01 -0600

    update firebase.json

--- web/pages.js
+++ web/pages.js
@@ -1,24 +1,12 @@
 module.exports = {
-    // "payment_cancel": {
-    //     "title": "Payment Canceled",
-    //     "path": "/cancel",
-    //     "name": "payment_cancel"
-    // },
-    // "subscription_confirmed": {
-    //     "title": "Subscription Confirmed",
-    //     "path": "/confirmed",
-    //     "name": "subscription_confirmed"
-    // },
-
-    "solo_proxy_auth": {
-        "title": "oAuth Proxy",
-        "path": "/solo-proxy-auth",
-        "name": "solo_proxy_auth"
+    'solo_proxy_auth': {
+        'title': 'oAuth Proxy',
+        'path': '/solo-proxy-auth',
+        'name': 'solo_proxy_auth',
     },
-
-    "single_page_index": {
-        "title": "Cactus",
-        "path": "/single-page-index",
-        "name": "single_page_index"
-    }
+    'single_page_index': {
+        'title': 'Cactus',
+        'path': '/single-page-index',
+        'name': 'single_page_index',
+    },
 }
