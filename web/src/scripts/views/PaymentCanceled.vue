ba428fc89faa3cd358bceb86df111fae51d080c3
Neil Poulin <neil.j.poulin@gmail.com>
2020-04-27 18:04:44 -0600

    payment cancel update

--- web/src/scripts/views/PaymentCanceled.vue
+++ web/src/scripts/views/PaymentCanceled.vue
@@ -6,7 +6,7 @@
         <section>
             <img class="graphic" src="assets/images/yogi.svg" alt=""/>
             <h1>Your payment has been canceled</h1>
-            <a class="button" href="/">Return Home</a>
+            <router-link class="button" to="/">Return Home</router-link>
         </section>
     </div>
 </template>
@@ -15,9 +15,32 @@
     import Vue from "vue";
 
     export default Vue.extend({
-        name: "PaymentCanceled"
+        name: "PaymentCanceled",
+        beforeMount(): void {
+            document.body.classList.add("cancel-component")
+        },
+        beforeDestroy(): void {
+            document.body.classList.remove("cancel-component")
+        }
     })
 </script>
+
+<style lang="scss">
+    @import "mixins";
+    @import "variables";
+
+    body.cancel-component {
+        background-color: lighten($lightGreen, 10%);
+
+        @include h(768) {
+            align-items: center;
+            display: flex;
+            height: 100vh;
+            justify-content: center;
+        }
+    }
+
+</style>
 
 <style scoped lang="scss">
     @import "mixins";
