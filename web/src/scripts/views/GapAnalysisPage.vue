9928bccf2b26b1aa2d2c4923d567cf44376187fe
Katie Blackman <katie@cactus.app>
2020-05-04 19:19:37 -0600

    more adding divs to create same look

--- web/src/scripts/views/GapAnalysisPage.vue
+++ web/src/scripts/views/GapAnalysisPage.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <assessment :assessment="assessment" @questionChanged="setQuestion"/>
+    <div class="coreValuesPage">
+        <div class="centered">
+            <h1>Gap Analysis</h1>
+            <assessment :assessment="assessment" @questionChanged="setQuestion"/>
+        </div>
     </div>
 </template>
 
@@ -48,5 +51,88 @@
 </script>
 
 <style scoped lang="scss">
+    @import "common";
+    @import "mixins";
+    @import "variables";
+
+    .coreValuesPage {
+        display: flex;
+        flex-flow: column nowrap;
+        min-height: 100vh;
+        justify-content: space-between;
+        overflow: hidden;
+        position: relative;
+
+        &:after {
+            background: url(assets/images/cvBlob.png) no-repeat;
+            content: "";
+            display: block;
+            height: 35rem;
+            overflow: hidden;
+            position: absolute;
+            left: 70%;
+            top: -26rem;
+            width: 40rem;
+        }
+
+        &:before {
+            background: url(assets/images/pinkVs.svg) no-repeat;
+            background-size: cover;
+            content: "";
+            display: block;
+            height: 17rem;
+            overflow: hidden;
+            position: absolute;
+            right: 70%;
+            top: 70%;
+            width: 18rem;
+        }
+
+        @include r(768) {
+            background-color: $beige;
+
+            &:after {
+                top: -22rem;
+                z-index: 0;
+            }
+        }
+
+        .centered {
+            flex-grow: 1;
+            max-width: 768px;
+            padding: 0;
+            position: relative;
+            text-align: left;
+            z-index: 1;
+        }
+
+        header, .centered {
+            width: 100%;
+        }
+
+        h1 {
+            display: none;
+
+            @include r(768) {
+                display: block;
+                margin: 6.4rem 0 1.6rem;
+            }
+        }
+
+        p {
+            margin-bottom: 1.6rem;
+            max-width: 64rem;
+        }
+    }
+
+    .primaryBtn {
+        display: block;
+        margin-top: 2.4rem;
+        width: 100%;
+
+        @include r(600) {
+            width: auto;
+        }
+    }
 
 </style>
