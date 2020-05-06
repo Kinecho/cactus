e28aa0b53007a9f10f43204c626971e1c56d0ff8
Katie Blackman <katie@cactus.app>
2020-05-06 14:18:00 -0600

    add index counter and modal - need to wire up, back button and next button styles

--- web/src/scripts/components/gapanalysis/Assessment.vue
+++ web/src/scripts/components/gapanalysis/Assessment.vue
@@ -2,21 +2,41 @@
     <div class="assessment-container">
         <progress-stepper :current="currentStepperIndex || 0" :total="stepperTotal"/>
         <div class="paddingContainer">
+            <h4>1 of 10</h4>
             <button aria-label="Close" title="Close" class="close tertiary icon">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
                     <path fill="#33CCAB" d="M8.414 7l5.293 5.293a1 1 0 0 1-1.414 1.414L7 8.414l-5.293 5.293a1 1 0 1 1-1.414-1.414L5.586 7 .293 1.707A1 1 0 1 1 1.707.293L7 5.586 12.293.293a1 1 0 0 1 1.414 1.414L8.414 7z"/>
                 </svg>
             </button>
+            <modal :show="showCloseConfirm" @close="showCloseConfirm = false">
+                <div class="close-confirm-modal paddingContainer" slot="body">
+                    <h3>Close assessment?</h3>
+                    <p class="subtext">Are you sure you want to close the assessment? Your progress will not be
+                        saved.</p>
+                    <div class="btnContainer">
+                        <button @click="showCloseConfirm = false">Continue assessment</button>
+                        <button class="secondary" @click="close">Close &amp; discard</button>
+                    </div>
+                </div>
+            </modal>
             <template v-if="!started" class="intro">
                 <p class="titleMarkdown">Take your gap analysis assessment to find out where you have gaps.</p>
                 <button class="btn primary" @click="start">Get Started</button>
             </template>
 
             <template v-else-if="currentQuestion && !finished && started">
+                <button class="backArrowbtn btn tertiary icon no-loading" @click="previous" v-if="previousEnabled">
+                    <svg class="backArrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                        <path d="M12.586 7L7.293 1.707A1 1 0 0 1 8.707.293l7 7a1 1 0 0 1 0 1.414l-7 7a1 1 0 1 1-1.414-1.414L12.586 9H1a1 1 0 1 1 0-2h11.586z"/>
+                    </svg>
+                </button>
                 <question :question="currentQuestion" :current-value="currentValue" @change="setValue"/>
-                <div class="actions">
-                    <button :disabled="!previousEnabled" class="no-loading" @click="previous">Previous</button>
-                    <button @click="next" :disabled="!nextEnabled" class="no-loading">Next</button>
+                <div class="cvActions">
+                    <button class="btn btn primary no-loading"
+                            @click="next"
+                            :disabled="!nextEnabled">
+                        Next
+                    </button>
                 </div>
             </template>
             <div v-else-if="finished && result">
@@ -39,6 +59,7 @@
     import CactusConfetti from "@components/CactusConfetti.vue";
     import Results from "@components/gapanalysis/Results.vue";
     import ProgressStepper from "@components/ProgressStepper.vue";
+    import Modal from "@components/Modal.vue";
 
     const logger = new Logger("gap/Assessment");
 
@@ -49,6 +70,7 @@
             Question,
             CactusConfetti,
             ProgressStepper,
+            Modal,
         }
     })
     export default class Assessment extends Vue {
