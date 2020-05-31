54d39d303df3b737e28f8ecdf7c17dc20a70b0e0
Katie Blackman <katie@cactus.app>
2020-05-30 22:23:05 -0600

    update core values card backgrounds

--- shared/src/util/CoreValuesUtil.ts
+++ shared/src/util/CoreValuesUtil.ts
@@ -17,31 +17,31 @@ export const blobs: CoreValuesBlob[] = [
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2F200602.png?alt=media&token=54c1ba6c-8445-4a5e-a387-21c08c719fb9"
     },
     {
-        backgroundColor: "#47445E",
+        backgroundColor: "#5e5a7c",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2F2006013.png?alt=media&token=f3e08a6f-9c0f-4558-8975-692f6f256a04"
     },
     {
-        backgroundColor: "#6590ED",
+        backgroundColor: "#47445E",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2F200531.png?alt=media&token=46d0e04e-818d-4d04-849a-a670f8764e52"
     },
     {
-        backgroundColor: "#294FA3",
+        backgroundColor: "#5e5a7c",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2F200526.png?alt=media&token=75a7de2c-a1a3-4b9a-8ff7-2c0639793354"
     },
     {
-        backgroundColor: "#294FA3",
+        backgroundColor: "#5e5a7c",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2F200524.4obqQP2rUQFS0IB0QrEC.png?alt=media&token=4d1a0983-91a8-47d6-b4d7-f0ca4084ff6f"
     },
     {
-        backgroundColor: "#294FA3",
+        backgroundColor: "#47445E",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2F200523.png?alt=media&token=a1ce93a0-699c-4d62-b8ed-65981e9f9484"
     },
     {
-        backgroundColor: "#47445E",
+        backgroundColor: "#5e5a7c",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2F200522.png?alt=media&token=c208bc62-c146-4a33-a70e-5006ba453a4d"
     },
     {
-        backgroundColor: "#0DADB1",
+        backgroundColor: "#5e5a7c",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/cactus-app-prod.appspot.com/o/flamelink%2Fmedia%2F200514.png?alt=media&token=6c91d271-93e7-438e-8bd9-20ec8d8fa37b"
     }
 ]
