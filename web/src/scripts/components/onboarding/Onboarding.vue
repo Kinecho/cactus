d8c8eb226d04e03a7418b4b8d6ccdc74a54c4352
Neil Poulin <neil.j.poulin@gmail.com>
2020-06-04 15:56:47 -0600

    add slide animation

--- web/src/scripts/components/onboarding/Onboarding.vue
+++ web/src/scripts/components/onboarding/Onboarding.vue
@@ -4,9 +4,12 @@
         <div class="progress-count">
             <span class="current">{{index + 1}}</span>&nbsp;of<span class="total">{{totalPages}}</span>
         </div>
-        <transition class="component-fade">
-            <Card :card="currentCard" :key="currentCard.id"/>
-        </transition>
+        <div class="card-container">
+            <transition-group :name="cardTransitionName" mode="in-out" appear>
+                <Card class="card" v-for="card in cards" :card="card" :key="card.id" v-show="card.id === currentCard.id"/>
+            </transition-group>
+        </div>
+
         <div class="footer actions">
             <button @click="previous" :disabled="!previousEnabled" class="no-loading">Previous</button>
             <button :disabled="!nextEnabled" @click="next" class="no-loading">Next</button>
@@ -37,6 +40,7 @@
 
         cards: OnboardingCardViewModel[] = OnboardingCardViewModel.createAll();
         index: number = 0;
+        cardTransitionName = "slide-left";
 
         get totalPages() {
             return this.cards.length;
@@ -69,12 +73,14 @@
 
         next() {
             if (this.nextEnabled) {
+                this.cardTransitionName = "slide-left";
                 this.index = Math.min(this.cards.length - 1, this.index + 1);
             }
         }
 
         previous() {
             if (this.previousEnabled) {
+                this.cardTransitionName = "slide-right";
                 this.index = Math.max(this.index - 1, 0);
             }
         }
@@ -86,12 +92,29 @@
     @import "mixins";
     @import "transitions";
 
+    .card-container {
+        position: relative;
+
+        .card {
+            position: absolute;
+            width: 100%;
+        }
+    }
+
     .footer {
         display: flex;
         flex-direction: row;
+        justify-content: center;
+        position: absolute;
+        background: $pink;
+        padding: 1rem;
+        bottom: 0;
+        left: 0;
+        right: 0;
 
         button {
             margin-right: 2rem;
+            flex: 1;
         }
     }
 </style>
