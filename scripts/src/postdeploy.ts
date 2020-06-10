9ee6aeb368a439df24d0c25bb162721cdf3e5243
Neil Poulin <neil.j.poulin@gmail.com>
2020-06-10 11:56:17 -0600

    update post deploy hooks

--- scripts/src/postdeploy.ts
+++ scripts/src/postdeploy.ts
@@ -1,4 +1,6 @@
 import chalk from "chalk";
+import { getCactusConfig, Project } from "@scripts/config";
+import AdminSlackService, { ChatMessage } from "@admin/services/AdminSlackService";
 
 // import * as simplegit from 'simple-git/promise';
 
@@ -14,21 +16,22 @@ import chalk from "chalk";
         resource = process.argv[2];
     }
 
-    console.log(chalk.green(`${resource} Deploy Finished! ${isProd ? "Prod" : "Stage"}`));
+    console.log(chalk.green(`Running ${ resource } ${ isProd ? "Prod" : "Stage" } postdeploy hook`));
 
     // const envName = isProd ? 'Prod' : 'Stage';
-    // const config = await getCactusConfig(isProd ? Project.PROD : Project.STAGE);
+    const config = await getCactusConfig(isProd ? Project.PROD : Project.STAGE);
+    console.log(`web domain from config: ${ config.web.domain }`);
 
-    // let resourceName = resource;
-    // if (resource.toLowerCase().trim() === "hosting") {
-        // resourceName = `<https://${config.web.domain}|Web>`
-    // }
-    // console.log(`got config: ${config.web.domain}`);
+    // let resourceSlackLink = resource;
+    let isHosting = false;
+    if (resource.toLowerCase().trim().includes("hosting") && !resource.toLowerCase().includes("storybook")) {
+        isHosting = true;
+        // resourceSlackLink = `<https://${ config.web.domain }|Web>`
+    }
+    console.log("is hosting deploy: ", isHosting);
 
+    AdminSlackService.initialize(config);
 
-    // AdminSlackService.initialize(config);
-    //
-    //
     // const username: string | undefined | null = await git.raw(['config', '--global', 'user.name']);
     // const email: string | undefined | null = await git.raw(['config', '--global', 'user.email']);
     //
@@ -40,25 +43,32 @@ import chalk from "chalk";
     // } else if (email) {
     //     byLine = email.trim();
     // }
-    //
-    // console.log(`username ${username} | email ${email}`);
+
+    // console.log(`username ${ username } | email ${ email }`);
 
     // const hash = await git.revparse(["HEAD"]);
     //
-    // const message: ChatMessage = {
-    //     text: "",
-    //     attachments: [{
-    //         text: `Finished deploying ${resourceName} to ${envName}`,
-    //         // text: `<https://sentry.io/organizations/kinecho/releases/${hash}|Sentry Release>: \`${hash}\`\n_started by ${byLine}_`,
-    //         color: "good",
-    //         ts: `${(new Date()).getTime() / 1000}`
-    //     }],
-    // };
 
 
-    // if (isProd && resource === "hosting") {
-    //     await AdminSlackService.getSharedInstance().sendGeneralMessage({text: `A new version of the website has been released! See it <https://cactus.app|here>`});
-    // }
-    // await AdminSlackService.getSharedInstance().sendCIMessage(message);
-
+    if (isProd && isHosting) {
+        const message: ChatMessage = {
+            text: "",
+            attachments: [{
+                text: `:white_check_mark: *Prod* has been updated. <https://cactus.app|Check it out>.`,
+                color: "good",
+                ts: `${ (new Date()).getTime() / 1000 }`
+            }],
+        };
+        await AdminSlackService.getSharedInstance().sendGeneralMessage(message);
+    } else if (isHosting) {
+        const message: ChatMessage = {
+            text: "",
+            attachments: [{
+                text: `*Stage* has been updated. <https://cactus-app-stage.web.app|Check it out>.`,
+                // color: "good",
+                ts: `${ (new Date()).getTime() / 1000 }`
+            }],
+        };
+        await AdminSlackService.getSharedInstance().sendGeneralMessage(message);
+    }
 })().then(() => console.log("Done")).catch(e => console.error("Failed to execute slack command", e));
