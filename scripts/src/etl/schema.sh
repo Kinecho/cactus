2cf97f276ad64c5141bba5ffb658c51263e57233
Neil Poulin <neil.j.poulin@gmail.com>
2020-06-11 19:18:30 -0600

    add member schema

--- scripts/src/etl/schema.sh
+++ scripts/src/etl/schema.sh
@@ -1,11 +1,11 @@
 PROJECT_ID="cactus-app-stage"
 DATASET_ID="cactus_stage"
-TABLE_ID="sent_prompts_schema"
+TABLE_ID="test_sent_prompts"
 npx @firebaseextensions/fs-bq-schema-views \
   --non-interactive \
   --project=${PROJECT_ID} \
   --dataset=${DATASET_ID} \
   --table-name-prefix=${TABLE_ID} \
-  --schema-files=./schema/sent_prompt.json
+  --schema-files=./schema/sent_prompts.json
 
 #  Schema files can be a list separated by a comma
