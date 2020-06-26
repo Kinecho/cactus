d4c1a9a3418ba6380e840fc63525ff95826be6b4
Neil Poulin <neil.j.poulin@gmail.com>
2020-06-26 13:30:47 -0600

    factor out tone analysis into separate component

--- web/src/scripts/components/InsightsCard.vue
+++ web/src/scripts/components/InsightsCard.vue
@@ -3,22 +3,7 @@
         <h2>Insights</h2>
         <p class="subtext">This is what your note reveals about your emotions.</p>
         <positivity-rating :sentiment-score="reflectionResponse.sentiment.documentSentiment"/>
-        <div class="sentimentAnalysis">
-            <nav class="tabs">
-                <a v-for="(tone, i) in tones" :key="`tone_${i}`" class="tone" :class="{selected: currentToneId === tone.toneId}" @click="currentToneIndex = i">{{tone.toneName}}</a>
-            </nav>
-            <div class="noteText">
-                <p v-for="(sentence, i) in reflectionResponse.toneAnalysis.sentencesTones"
-                        :key="`sentence_${i}`"
-                        :class="{highlight: sentence.tones && sentence.tones.some(t => t.toneId === currentToneId)}"
-                >
-                    <span class="debug">({{sentence.tones.map(t => t.toneName).join(', ')}})</span>
-                    <br/>
-                    {{sentence.text}}
-
-                </p>
-            </div>
-        </div>
+        <tone-analysis :tone-result="reflectionResponse.toneAnalysis"/>
         <button class="contButton">Continue</button>
         <button class="infoButton tertiary icon" @click="showModal">
             <svg-icon icon="info" class="infoIcon"/>
@@ -38,9 +23,11 @@
     import ReflectionResponse from "@shared/models/ReflectionResponse";
     import { ToneID, ToneScore } from "@shared/api/ToneAnalyzerTypes";
     import PositivityRating from "@components/PositivityRating.vue";
+    import ToneAnalysis from "@components/ToneAnalysis.vue";
 
     @Component({
         components: {
+            ToneAnalysis,
             ToneAnalyzerModal,
             SvgIcon,
             PositivityRating,
@@ -50,19 +37,6 @@
 
         @Prop({ type: Object as () => ReflectionResponse, required: true })
         reflectionResponse!: ReflectionResponse;
-
-        get tones(): ToneScore[] {
-            return this.reflectionResponse.toneAnalysis?.documentTone?.tones ?? [];
-        }
-
-        currentToneIndex = 0;
-
-        get currentToneId(): ToneID | null {
-            if (!this.tones || this.tones.length === 0) {
-                return null;
-            }
-            return this.tones[Math.min(this.currentToneIndex, this.tones.length - 1)]?.toneId ?? null;
-        };
 
         modalVisible: boolean = false;
 
@@ -106,82 +80,6 @@
             font-size: 1.8rem;
             margin-bottom: 4rem;
         }
-    }
-
-    .sentimentAnalysis {
-        @include shadowbox;
-        margin: 0 -3.2rem;
-        position: relative;
-
-        @include r(374) {
-            margin: 0 -2rem;
-        }
-        @include r(600) {
-            margin: 0 -2.4rem;
-        }
-
-        &:after {
-            background-image: linear-gradient(to right, rgba(255, 255, 255, 0), $white);
-            content: "";
-            height: 4rem;
-            position: absolute;
-            right: 0;
-            top: 1.2rem;
-            width: 4rem;
-            z-index: 1;
-        }
-    }
-
-    .tabs {
-        background-color: $white;
-        border-radius: 1.2rem 1.2rem 0 0;
-        cursor: pointer;
-        display: flex;
-        font-weight: bold;
-        overflow-x: auto;
-        overflow-y: hidden;
-        position: sticky;
-        top: 0;
-    }
-
-    .tone {
-        padding: 2rem 1.6rem;
-
-        &:first-child {
-            padding-left: 3.2rem;
-            @include r(374) {
-                padding-left: 2.4rem;
-            }
-        }
-
-        &:hover {
-            cursor: pointer;
-        }
-
-        &.selected {
-            color: $darkestGreen;
-        }
-    }
-
-    .noteText {
-        padding: 0 3.2rem 1.6rem;
-
-        @include r(374) {
-            padding: 0 2.4rem 3.2rem;
-        }
-        @include r(600) {
-            padding: 0 2.4rem 3.2rem;
-        }
-
-        p {
-            margin-bottom: 1.6rem;
-        }
-    }
-
-    .highlight {
-        background-color: lighten($royal, 20%);
-        margin: 0 -.4rem 1.6rem;
-        padding: .4rem;
     }
 
     .contButton {
---
+++ web/src/scripts/components/ToneAnalysis.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="toneAnalysis" v-if="toneResult">
+        <nav class="tabs">
+            <a v-for="(tone, i) in tones"
+                    :key="`tone_${i}`" class="tone"
+                    :class="{selected: currentToneId === tone.toneId}"
+                    @click="currentToneIndex = i">
+                {{tone.toneName}}
+            </a>
+        </nav>
+        <div class="noteText">
+            <p v-for="(sentence, i) in toneResult.sentencesTones"
+                    :key="`sentence_${i}`"
+                    :class="{highlight: sentence.tones && sentence.tones.some(t => t.toneId === currentToneId)}"
+            >
+                <span class="debug">({{sentence.tones.map(t => t.toneName).join(', ')}})</span>
+                <br/>
+                {{sentence.text}}
+
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import Component from "vue-class-component"
+    import { ToneID, ToneResult, ToneScore } from "@shared/api/ToneAnalyzerTypes";
+    import { Prop } from "vue-property-decorator";
+
+    @Component
+    export default class ToneAnalysis extends Vue {
+        name = "ToneAnalysis";
+
+        @Prop({ type: Object as () => ToneResult, required: false, default: null })
+        toneResult!: ToneResult | null;
+
+
+        get tones(): ToneScore[] {
+            return this.toneResult?.documentTone?.tones ?? [];
+        }
+
+        currentToneIndex = 0;
+
+        get currentToneId(): ToneID | null {
+            if (!this.tones || this.tones.length === 0) {
+                return null;
+            }
+            return this.tones[Math.min(this.currentToneIndex, this.tones.length - 1)]?.toneId ?? null;
+        };
+
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "variables";
+    @import "mixins";
+
+    .toneAnalysis {
+        @include shadowbox;
+        margin: 0 -3.2rem;
+        position: relative;
+
+        @include r(374) {
+            margin: 0 -2rem;
+        }
+        @include r(600) {
+            margin: 0 -2.4rem;
+        }
+
+        &:after {
+            background-image: linear-gradient(to right, rgba(255, 255, 255, 0), $white);
+            content: "";
+            height: 4rem;
+            position: absolute;
+            right: 0;
+            top: 1.2rem;
+            width: 4rem;
+            z-index: 1;
+        }
+    }
+
+    .tabs {
+        background-color: $white;
+        border-radius: 1.2rem 1.2rem 0 0;
+        cursor: pointer;
+        display: flex;
+        font-weight: bold;
+        overflow-x: auto;
+        overflow-y: hidden;
+        position: sticky;
+        top: 0;
+    }
+
+    .tone {
+        padding: 2rem 1.6rem;
+
+        &:first-child {
+            padding-left: 3.2rem;
+            @include r(374) {
+                padding-left: 2.4rem;
+            }
+        }
+
+        &:hover {
+            cursor: pointer;
+        }
+
+        &.selected {
+            color: $darkestGreen;
+        }
+    }
+
+    .noteText {
+        padding: 0 3.2rem 1.6rem;
+
+        @include r(374) {
+            padding: 0 2.4rem 3.2rem;
+        }
+        @include r(600) {
+            padding: 0 2.4rem 3.2rem;
+        }
+
+        p {
+            margin-bottom: 1.6rem;
+        }
+    }
+
+    .highlight {
+        background-color: lighten($royal, 20%);
+        margin: 0 -.4rem 1.6rem;
+        padding: .4rem;
+    }
+
+</style>
