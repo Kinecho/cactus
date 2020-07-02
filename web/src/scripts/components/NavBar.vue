dbb93fe9719e45628000f06cbec4b3be3159a7a0
Katie Blackman <katie@cactus.app>
2020-07-02 14:32:18 -0600

    add button to navbar with placeholder link

--- web/src/scripts/components/NavBar.vue
+++ web/src/scripts/components/NavBar.vue
@@ -44,7 +44,11 @@
                     </svg>
                     <span class="navLabel">{{copy.navigation.INSIGHTS}}</span>
                 </router-link>
-                <dropdown-menu :items="links" v-if="loggedIn" :displayName="displayName" :email="email">
+                 <router-link class="composeBtn" :to="journalHref" v-if="loggedIn">
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><title>Compose</title><path d="M17 3a2.828 2.828 0 114 4L7.5 20.5 2 22l1.5-5.5L17 3z"/></svg>
+                    <span>{{copy.navigation.COMPOSE}}</span>
+                </router-link>
+               <dropdown-menu :items="links" v-if="loggedIn" :displayName="displayName" :email="email">
                     <div class="navbar-avatar-container" slot="custom-button">
                         <div v-if="!profileImageUrl" class="initials">{{initials}}</div>
                         <img v-if="profileImageUrl" :alt="(displayName || email) + `'s Profile Image`" :src="profileImageUrl"/>
@@ -423,6 +427,62 @@
 
             @include r(600) {
                 color: $darkestGreen;
+                display: block;
+            }
+        }
+    }
+
+    .composeBtn {
+        align-items: center;
+        background-color: $green;
+        border: 0;
+        border-radius: 50%;
+        bottom: 1.6rem;
+        box-shadow: 0 10px 20px rgba(0, 0, 0,0.1), 0 6px 6px rgba(0, 0, 0, 0.1);
+        display: flex;
+        height: 5.6rem;
+        justify-content: center;
+        padding: .8rem;
+        position: fixed;
+        right: 1.6rem;
+        transition: background-color .3s;
+        width: 5.6rem;
+        
+        svg {
+            height: 2rem;
+            width: 2rem;
+        }
+
+        span {
+            display: none;
+        }
+
+        &:hover,
+        &:active {
+            background-color: $darkGreen;
+        }
+
+        @include r(600) {
+            border-radius: 3rem;
+            box-shadow: none;
+            bottom: auto;
+            color: white;
+            font: 1.6rem $font-stack;
+            height: auto;
+            padding: .7rem 1.6rem .9rem;
+            position: static;
+            right: auto;
+            text-decoration: none;
+            transition: background-color .3s;
+            width: auto;
+
+            svg {
+                height: 1.6rem;
+                margin-right: .4rem;
+                width: 1.6rem;
+            }
+
+            span {
                 display: block;
             }
         }
