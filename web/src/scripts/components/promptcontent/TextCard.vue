0d461e0406d1d8954e120bbe2abfb01b474f6614
Katie Blackman <katie@cactus.app>
2020-07-06 13:15:26 -0600

    padding, css for text card

--- web/src/scripts/components/promptcontent/TextCard.vue
+++ web/src/scripts/components/promptcontent/TextCard.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="prompt-content-card">
-        <span>text card</span>
-        <markdown-text :source="text" v-if="text"/>
+        <div class="text-card">
+            <div class="textBox">
+                <span>text card</span>
+                <markdown-text :source="text" v-if="text"/>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -43,6 +47,48 @@
             //do not add margin: auto here as it makes the cards jumpy
             padding: 5.6rem 3.2rem;
             width: 100%;
+        }
+    }
+
+    .text-card {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        min-height: 80vh;
+        padding: 0 .8rem;
+
+        @include r(374) {
+            justify-content: center;
+            margin: 0 auto;
+            max-width: 48rem;
+            padding: 0 2.4rem;
+        }
+        @include r(768) {
+            align-items: center;
+            flex-direction: row;
+            justify-content: flex-start;
+            max-width: none;
+            padding: 0 6.4rem;
+        }
+    }
+
+    .textBox {
+        margin-bottom: 5.6rem;
+
+        @include r(768) {
+            margin-bottom: 0;
+            padding-right: 6.4rem;
+            width: 66%;
+        }
+    }
+
+    .image {
+        height: auto;
+        width: 100%;
+
+        @include r(768) {
+            align-self: center;
+            max-width: 33%;
         }
     }
 </style>
