abc6ea5c1a54349a63fd48f78e998a00862c453b
Katie Blackman <katie@cactus.app>
2020-07-07 19:59:15 -0600

    fix up those last buttons

--- web/src/scripts/components/promptcontent/PromptContent.vue
+++ web/src/scripts/components/promptcontent/PromptContent.vue
@@ -24,8 +24,8 @@
                 </template>
                 <template v-slot:actions>
                     <template v-if="isLastCard">
-                        <button class="button actions" @click="closePrompt">Done</button>
-                        <button v-if="hasNote" class="button actions tertiary shareNote" @click="showShareNote = true">
+                        <button class="button" @click="closePrompt">Done</button>
+                        <button v-if="hasNote" class="button tertiary shareNote" @click="showShareNote = true">
                             <svg-icon icon="share"/>
                             Share Note
                         </button>
@@ -486,6 +486,16 @@
             .card-container {
                 flex-grow: 0;
             }
+        }
+    }
+
+    .shareNote {
+        color: $darkerGreen;
+
+        .icon {
+            height: 1.8rem;
+            margin-right: .8rem;
+            width: 1.8rem;
         }
     }
 
