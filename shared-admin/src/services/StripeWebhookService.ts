2dea7d19b6f429fe6268fd182a3f8af0c345b451
Neil Poulin <neil.j.poulin@gmail.com>
2020-07-13 15:23:38 -0600

    update attributes on revenuecat user after checkout completes

--- shared-admin/src/services/StripeWebhookService.ts
+++ shared-admin/src/services/StripeWebhookService.ts
@@ -22,7 +22,6 @@ import { destructureDisplayName, isBlank } from "@shared/util/StringUtil";
 import StripeService from "@admin/services/StripeService";
 import { formatDateTime } from "@shared/util/DateUtil";
 import AdminRevenueCatService from "@admin/services/AdminRevenueCatService";
-import RevenueCatService from "../../../web/src/scripts/services/RevenueCatService";
 
 const logger = new Logger("StripeWebhookService");
 
@@ -259,7 +258,7 @@ export default class StripeWebhookService {
         await Promise.all([
             AdminPaymentService.getSharedInstance().save(payment),
             AdminCactusMemberService.getSharedInstance().save(cactusMember, { setUpdatedAt: false }),
-            RevenueCatService.shared.updateAttributes(cactusMember),
+            AdminRevenueCatService.shared.updateAttributes(cactusMember),
         ])
         return { statusCode: 200, body: `Member ${ cactusMember.email } was upgraded to ${ cactusSubscription.tier }` };
     };
