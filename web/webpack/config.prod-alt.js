d62bf3627d33074fe55bd0a119697e9d9b4f8b0b
Neil Poulin <neil.j.poulin@gmail.com>
2020-07-27 17:00:29 -0600

    add anti flicker script

--- web/webpack/config.prod-alt.js
+++ web/webpack/config.prod-alt.js
@@ -2,7 +2,7 @@ const config = require('./config.prod')
 module.exports = {
     ...config,
     // START: Use stage values for analytics stuff
-    __GOOGLE_ANALYTICS_ID__: 'UA-127159143-6',
+    __GOOGLE_ANALYTICS_ID__: 'UA-127159143-5',
     __GOOGLE_OPTIMIZE_ID__: 'OPT-PWF92J5',
     __FACEBOOK_PIXEL_ID__: '1028725524153114',
     // END: Use stage values for analytics stuff
