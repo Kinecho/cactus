45d9dffb9f0728a40b5a4eb65cb78fa0d8c44bd9
Neil Poulin <neil.j.poulin@gmail.com>
2020-08-11 15:06:33 -0600

    add milestone text

--- web/src/scripts/components/insights/MilestoneTypes.ts
+++ web/src/scripts/components/insights/MilestoneTypes.ts
@@ -13,3 +13,25 @@ export const defaultMilestones = (): Milestone[] => {
         { type: "flag" },
     ];
 }
+
+
+export function getMilestoneText(days: number): { title: string, message: string } {
+    switch (days) {
+        case 1:
+            return {title: "Well done", message: "Tomorrow you'll receive a new prompt to continue your journal of self-understanding.\n\nKeep it up and you'll see how you positively change over time."}
+        case 2:
+            return {title: "Nice work", message: "Just one m ore reflection to reveal your positivity insight.\n\nWe are excited to be your co-pilot on your journey of self-understanding"}
+        case 3:
+            return {title: "You did it", message: "You've reflected three times and now your positivity insight is ready."}
+        case 4:
+            return {title: "Great job", message: "You're more than half-way to your seven day goal. Keep it up."}
+        case 5:
+            return {title: "Nice work", message: "You're almost there; just a coupe more reflections."}
+        case 6:
+            return {title: "Well done", message: "One reflection away from unlocking your emotions insight."}
+        case 7:
+            return {title: "You did it", message: "You made your 7-day goal. It's time to reveal your new insight."}
+        default:
+            return {title: "Well done", message: "Tomorrow you'll receive a new prompt to continue your journal of self-understanding.\n\nKeep it up and you'll see how you positively change over time."}
+    }
+}
--- web/src/scripts/components/onboarding/OnboardingStreakCard.vue
+++ web/src/scripts/components/onboarding/OnboardingStreakCard.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="text-card">
-        <markdown-text v-if="markdownText" :source="markdownText"/>
+        <h2>
+            <markdown-text :source="message.title"/>
+        </h2>
+        <p><markdown-text :source="message.message"/></p>
         <milestone-progress :total-reflections="totalReflections"/>
         <div class="actions" v-if="card.buttons && card.buttons.length > 0">
             <ActionButton v-for="(button, index) in card.buttons"
@@ -13,101 +16,101 @@
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
-    import Component from "vue-class-component"
-    import OnboardingCardViewModel from "@components/onboarding/OnboardingCardViewModel";
-    import { Prop } from "vue-property-decorator";
-    import MarkdownText from "@components/MarkdownText.vue";
-    import OnboardingActionButton from "@components/OnboardingActionButton.vue";
-    import CactusMember from "@shared/models/CactusMember";
-    import { CoreValue } from "@shared/models/CoreValueTypes";
-    import SvgIcon from "@components/SvgIcon.vue";
-    import MilestoneProgress from "@components/insights/MilestoneProgress.vue";
+import Vue from "vue";
+import Component from "vue-class-component"
+import OnboardingCardViewModel from "@components/onboarding/OnboardingCardViewModel";
+import { Prop } from "vue-property-decorator";
+import MarkdownText from "@components/MarkdownText.vue";
+import OnboardingActionButton from "@components/OnboardingActionButton.vue";
+import CactusMember from "@shared/models/CactusMember";
+import { CoreValue } from "@shared/models/CoreValueTypes";
+import SvgIcon from "@components/SvgIcon.vue";
+import MilestoneProgress from "@components/insights/MilestoneProgress.vue";
+import { getMilestoneText } from "@components/insights/MilestoneTypes";
 
-    @Component({
-        components: {
-            MilestoneProgress,
-            MarkdownText,
-            SvgIcon,
-            ActionButton: OnboardingActionButton,
-        }
-    })
-    export default class OnboardingStreakCard extends Vue {
-        name = "OnboardingStreakCard.vue";
-
-        @Prop({ type: Object as () => OnboardingCardViewModel, required: true })
-        card!: OnboardingCardViewModel;
-
-        @Prop({ type: Object as () => CactusMember, required: true })
-        member!: CactusMember;
-
-        @Prop({ type: String, required: false, default: null })
-        selectedInsightWord!: string | null;
-
-        @Prop({type: String as () => CoreValue, required: false, default: null})
-        selectedCoreValue!: CoreValue | null;
-
-        @Prop({ type: Boolean, default: true })
-        done!: boolean;
-
-        get totalReflections(): number {
-            return 1
-        }
-
-        get markdownText() {
-            return this.card.getMarkdownText({ selectedInsight: this.selectedInsightWord, selectedCoreValue: this.selectedCoreValue })
-        }
-
-        closeOnboarding() {
-            this.$emit('close', true)
-        }
+@Component({
+    components: {
+        MilestoneProgress,
+        MarkdownText,
+        SvgIcon,
+        ActionButton: OnboardingActionButton,
     }
+})
+export default class OnboardingStreakCard extends Vue {
+    name = "OnboardingStreakCard.vue";
+
+    @Prop({ type: Object as () => OnboardingCardViewModel, required: true })
+    card!: OnboardingCardViewModel;
+
+    @Prop({ type: Object as () => CactusMember, required: true })
+    member!: CactusMember;
+
+    @Prop({ type: String, required: false, default: null })
+    selectedInsightWord!: string | null;
+
+    @Prop({ type: String as () => CoreValue, required: false, default: null })
+    selectedCoreValue!: CoreValue | null;
+
+    get totalReflections(): number {
+        return 1
+    }
+
+
+    get message(): { title: string, message: string } {
+        return getMilestoneText(this.totalReflections)
+        // return this.card.getMarkdownText({ selectedInsight: this.selectedInsightWord, selectedCoreValue: this.selectedCoreValue })
+    }
+
+    closeOnboarding() {
+        this.$emit('close', true)
+    }
+}
 </script>
 
 <style scoped lang="scss">
-    @import "variables";
-    @import "mixins";
+@import "variables";
+@import "mixins";
 
-    .text-card {
-        display: flex;
-        flex-direction: column;
-        min-height: 80vh;
-        padding: 0 .8rem;
+.text-card {
+  display: flex;
+  flex-direction: column;
+  min-height: 80vh;
+  padding: 0 .8rem;
 
-        @include r(374) {
-            justify-content: center;
-            margin: 0 auto;
-            max-width: 48rem;
-        }
-        @include r(768) {
-            max-width: none;
-            padding: 0 6.4rem;
-        }
+  @include r(374) {
+    justify-content: center;
+    margin: 0 auto;
+    max-width: 48rem;
+  }
+  @include r(768) {
+    max-width: none;
+    padding: 0 6.4rem;
+  }
+}
+
+.actions {
+  bottom: 2.4rem;
+  left: 2.4rem;
+  position: fixed;
+  right: 2.4rem;
+
+  @include r(768) {
+    margin-top: 3.2rem;
+    position: static;
+  }
+
+  button {
+    margin-left: 50%;
+    max-width: 48rem;
+    transform: translateX(-50%);
+    width: 100%;
+
+    @include r(768) {
+      margin: 0;
+      min-width: 24rem;
+      transform: none;
+      width: auto;
     }
-
-    .actions {
-        bottom: 2.4rem;
-        left: 2.4rem;
-        position: fixed;
-        right: 2.4rem;
-
-        @include r(768) {
-            margin-top: 3.2rem;
-            position: static;
-        }
-
-        button {
-            margin-left: 50%;
-            max-width: 48rem;
-            transform: translateX(-50%);
-            width: 100%;
-
-            @include r(768) {
-                margin: 0;
-                min-width: 24rem;
-                transform: none;
-                width: auto;
-            }
-        }
-    }
+  }
+}
 </style>
