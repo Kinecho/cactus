9a25349cf358611db522a803536129d3b9b6cae3
Neil Poulin <neil.j.poulin@gmail.com>
2020-08-11 16:08:41 -0600

    update from ryan's updates

--- web/src/scripts/components/insights/MilestoneTypes.ts
+++ web/src/scripts/components/insights/MilestoneTypes.ts
@@ -18,20 +18,29 @@ export const defaultMilestones = (): Milestone[] => {
 export function getMilestoneText(days: number): { title: string, message: string } {
     switch (days) {
         case 1:
-            return {title: "Well done", message: "Tomorrow you'll receive a new prompt to continue your journal of self-understanding.\n\nKeep it up and you'll see how you positively change over time."}
+            return {
+                title: "Well done",
+                message: "Tomorrow you'll receive a new prompt to continue your journey of self-understanding.\n\nKeep it up and you'll soon see positive change."
+            }
         case 2:
-            return {title: "Nice work", message: "Just one more reflection to reveal your positivity insight.\n\nWe are excited to be your co-pilot on your journey of self-understanding"}
+            return {
+                title: "Nice work",
+                message: "Just one more reflection to reveal your positivity insight.\n\nWe are excited to be your co-pilot on this journey of self-understanding"
+            }
         case 3:
-            return {title: "You did it", message: "You've reflected three times and now your positivity insight is ready."}
+            return { title: "You did it", message: "You've reflected three times. Your positivity insight is ready." }
         case 4:
-            return {title: "Great job", message: "You're more than half-way to your seven day goal. Keep it up."}
+            return { title: "Great job", message: "You're more than halfway to your seven-day goal. Keep it up." }
         case 5:
-            return {title: "Nice work", message: "You're almost there; just a coupe more reflections."}
+            return { title: "Nice work", message: "You're almost there; just a couple more reflections." }
         case 6:
-            return {title: "Well done", message: "One reflection away from unlocking your emotions insight."}
+            return { title: "Well done", message: "You're one reflection away from unlocking your emotions insight." }
         case 7:
-            return {title: "You did it", message: "You made your 7-day goal. It's time to reveal your new insight."}
+            return { title: "You did it", message: "You made your 7-day goal. It's time to reveal your new insight." }
         default:
-            return {title: "Well done", message: "Tomorrow you'll receive a new prompt to continue your journal of self-understanding.\n\nKeep it up and you'll see how you positively change over time."}
+            return {
+                title: "Well done",
+                message: "Tomorrow you'll receive a new prompt to continue your journey of self-understanding.\n\nKeep it up and you'll soon see positive change."
+            }
     }
 }
